a9b5fceae5572e1e88c8138506b3384949b44306
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-07-02 11:33:01 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,8 @@ client.on("ready", () => {
 client.on("message", message => {
    if(message.content === "!help") {
       message.channel.send("Commands: **!info**, **!help**, **!roast @USERNAME** ");
-   }else if(message.context === "!info") {
+   }
+   if(message.context === "!info") {
       message.channel.send("Roast-Bot was created by Ole113 on 2018-06-26 and is version BETA 1.0.2. For more information visit https://github.com/Ole113/Roast-Bot");
    }
    
