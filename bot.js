5f937dac9f532c9874f12c6c5823e0f95aeb3122
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-07-02 09:46:56 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,13 @@
 const Discord = require(“discord.js”);
-
 const client = new Discord.Client();
 
- 
-
 client.on(“ready”, () => {
-
-    console.log(“Ready”);
-
+   console.log(“I am ready!”);
 });
-
- 
-
 client.on(“message”, message => {
-
-    if (message.content === '!help') {
-
-       message.reply(“Commands: !info, !roast [PERSON], !help”);
-
-       }
-
+   if (message.content ===“ping”) {
+      message.reply(“pong”);
+   }
 });
-
 
 client.login(process.env.BOT_TOKEN);
