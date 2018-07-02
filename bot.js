bd72a71a7ff5fab2577fced2a207636a045e37f9
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-07-02 11:23:39 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,10 +9,10 @@ client.on("message", message => {
       message.channel.send("Commands: **!info**, **!help**, **!roast @USERNAME** ");
    }
    
-   if(message.context === "!info"){
-        message.reply("Roast-Bot was created by Ole113 on 2018-06-26 and is version BETA 1.0.2. For more information visit https://github.com/Ole113/Roast-Bot");
+   if(message.context === "!info") {
+        message.channel.send("Roast-Bot was created by Ole113 on 2018-06-26 and is version BETA 1.0.2. For more information visit https://github.com/Ole113/Roast-Bot");
    }
    
 });
-
+//message.reply
 client.login(process.env.BOT_TOKEN);
