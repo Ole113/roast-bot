db5e5dabe949f49d77b8b2ba8ad1826e42d211f2
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-07-02 18:51:07 -0600

    RichEmbed added

--- bot.js
+++ bot.js
@@ -6,14 +6,24 @@ client.on("ready", () => {
 });
 client.on("message", message => {
    if(message.content === "!help") {
-      message.channel.send("Commands: **!info**, **!help**, **!roast @USERNAME** ");
+      return message.channel.send("Commands: **!info**, **!help**, **!roast @USERNAME** ");
    }
    /*
    if(message.content === "!info") {
     message.channel.send("Roast-Bot was created by Ole113 on 2018-06-26 and is version BETA 1.0.2. For more information visit https://github.com/Ole113/Roast-Bot");
    }
    */
+   if(message.content === "!info"){
 
+    let bicon = client.user.displayAvatarURL;
+       let botembed = new Discord.RichEmbed()
+       .setDescription("Roast-Bot Information")
+       .setColor("#ff3300")
+       .setThumbnail(bicon)
+       .addField("Bot Name", "Roast-Bot");
+       
+       return message.channel.send(botembed);
+   }
 });
 //message.reply
 client.login(process.env.BOT_TOKEN);
