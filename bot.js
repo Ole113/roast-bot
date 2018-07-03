56957e8fc0ed72d92e989346e08ff82b7edbe633
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-07-02 20:35:36 -0600

    test

--- bot.js
+++ bot.js
@@ -5,15 +5,9 @@ client.on("ready", () => {
    console.log("I am ready!");
 });
 client.on("message", message => {
-   if(message.content === "!help") {
+   if(message.content.startsWith() == "!help") {
       return message.channel.send("Commands: **!info**, **!help**, **!roast @USERNAME** ");
-   }
-   /*
-   if(message.content === "!info") {
-    message.channel.send("Roast-Bot was created by Ole113 on 2018-06-26 and is version BETA 1.0.2. For more information visit https://github.com/Ole113/Roast-Bot");
-   }
-   */
-   if(message.content === "!info"){
+   } else if(message.content.startsWith() == "!info"){
 
     let bicon = client.user.displayAvatarURL;
        let botembed = new Discord.RichEmbed()
