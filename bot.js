217655623512d560954ddd7dcf52c5f1dbe07552
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-07-05 09:54:38 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,9 +68,12 @@ client.on("message", message => {
    } else if(message.content === "!info"){
         return message.channel.send("Roast-Bot was created on 2018-06-26 by Ole113. For more information visit https://github.com/Ole113/Roast-Bot");
    } else if (message.content.startsWith("!roast")) {
-	const random = Math.ceil(Math.random() * 54 - 1);
-	return message.channel.send(roasts[random].roast);
-
+	var i = 0;
+	return message.channel.send(roasts[i].roast);
+	i++;
+	if(i == roasts.length){
+		i = 0;	
+	}
    }
 });
 //message.reply
