0a7dfb5e6358baf15b9cb2446c85b16395bb4689
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-07-05 08:58:28 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,8 +58,8 @@ const roasts = [
 	{"roast":"The last time I saw a face like yours I fed it a banana"}
 ];
 const random = Math.ceil(Math.random() * 54);
-mention = message.mentions.users.first();
-if(message.author.bot) return;
+//mention = message.mentions.users.first();
+//if(message.author.bot) return;
 client.on("ready", () => {
    console.log("Ready");
 });
@@ -68,13 +68,13 @@ client.on("message", message => {
       return message.channel.send("Commands: **!info**, **!help**, **!roast @USERNAME** ");
    } else if(message.content === "!info"){
         return message.channel.send("Roast-Bot was created on 2018-06-26 by Ole113. For more information visit https://github.com/Ole113/Roast-Bot");
-   } else if (message.content.startsWith("!roast")) {
+   } /*else if (message.content.startsWith("!roast")) {
         if(mention == null){ return; }
 	message.delete();
         mentionMessage = message.content.slice(7);
         mention.sendMessage(mentionMessage);
         message.channel.send("done");
-}
+} */
 });
 //message.reply
 client.login(process.env.BOT_TOKEN);
