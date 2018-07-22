f305f12c7c644cec311bb8aaaace8c9a1d80dc92
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-07-22 16:30:00 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,7 +73,7 @@ client.on("message", message => {
        .setDescription("Bot Information:")
        .setThumbnail(icon);
        //.addField("Created On:", client.user.createdAt);
-       //.addField("Name:", client.user.username)
+       // .addField("Name:", client.user.username)
        return message.channel.send(embed);
        //return message.channel.send("Roast-Bot was created on 2018-06-26 by Ole113. For more information visit https://github.com/Ole113/Roast-Bot");
     } else if(message.content === "r!roast") {
