7f446cf7ef2bfd8e7d9206a68f021f8e8f5d3beb
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-07-30 14:27:30 -0600

    period update

--- bot.js
+++ bot.js
@@ -110,7 +110,7 @@ client.on("message", message => {
 		.addField("welcome-leave-log", "To use the Roast-Bot welcome-leave-log make a channel named \"welcome-leave-log\".")
 		.addBlankField()
 		.addField("Roast-Bot Development Server:", "If you still need help, have any questions or feedback join the Roast-Bot help server. \n \n https://discord.gg/fuDF42D")
-		.setFooter("v1.2.4, for release notes join the Roast-Bot help server");
+		.setFooter("v1.2.4, for release notes join the Roast-Bot help server.");
 		return message.channel.send(help_embed);
     } else if(message.content === "r!bot"){
         let bot_icon = client.user.displayAvatarURL;
