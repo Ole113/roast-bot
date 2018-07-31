74a98121891dbd75bed08d9fcc8d5488bac71141
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-07-30 19:18:26 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -130,8 +130,8 @@ client.on("message", message => {
 		const reply = word.slice(8, word.length);
 
 		return message.channel.send(reply + ", " + roasts[random - 1].roast);
-    } else if(message.content === "r!roast"){
-     		return message.channel.send(roasts[random - 1].roast);
+    /*} else if(message.content === "r!roast"){
+     		return message.channel.send(roasts[random - 1].roast); */
 }else if(message.content === "r!invite") {
     	message.channel.send("Invite Link: https://discordbots.org/bot/461361233644355595");
     } else if(message.content === "r!server"){
