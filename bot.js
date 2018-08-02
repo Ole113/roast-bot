7337cdf6721541bd47c23cd9dfda9e0ed849a630
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-02 13:25:30 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -172,7 +172,7 @@ client.on("message", message => {
 			return message.channel.send(`Cleared ${number} messages.`)
 		});
 	} else if(message.content === "r!clear"){
-    return message.channel.send("Incorrect usage of r!clear, please provide how many messages you want to be deleted. The correct usage is r!clear #OF_MESSAGES.
+    return message.channel.send("Incorrect usage of r!clear, please provide how many messages you want to be deleted. The correct usage is r!clear #OF_MESSAGES.");
   }
 	/*
 	else if(message.content === "r!weather") {
