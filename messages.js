11d82fe049f95e7e6c08913e8532f151f80e0204
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-02 11:05:15 -0600

    fix

--- messages.js
+++ messages.js
@@ -3,7 +3,7 @@ const db = require('quick.db');
 exports.run = (bot, message, args, func) => {
 
     db.fetchObject(message.author.id + message.guild.id).then(i => {
-        db.fetchObject(`userLevel_${message.author.id + message.guild.id}`).then(o => { 
+        db.fetch(`userLevel_${message.author.id + message.guild.id}`).then(o => { 
             message.channel.send('Messages sent: `' + (i.value + 1) + '`\nLevel: `' + o.value + '`'); 
         })
     })
