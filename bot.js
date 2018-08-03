5e5011f482878099f774787e0c0faeb80839b442
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-02 21:31:21 -0600

    test

--- bot.js
+++ bot.js
@@ -154,8 +154,8 @@ client.on("message", message => {
 		return message.channel.send(server_embed);
 	} else if(message.content === "r!meme") {
 		const random_memes = Math.ceil(Math.random() * 107);
-		let meme_embed = new Discord.RichEmbed()
-		return message.channel.send({files: [`Images/meme${random_memes}.PNG`]} + ` \nMeme #${random_memes} <:roast_circle:474755210485563404>`)
+	
+		return message.channel.send(message.channel.send({files: [`Images/meme${random_memes}.PNG`]}) + ` \nMeme #${random_memes} <:roast_circle:474755210485563404>`)
 	} else if(message.content.startsWith("r!clear")){
 		const word = message.content;
 		const number = word.slice(7, word.length);
