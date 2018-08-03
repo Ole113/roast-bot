51b533c39024fb24873b91b29c99ead4c8fe519e
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-03 13:26:15 -0600

    fix

--- bot.js
+++ bot.js
@@ -130,7 +130,7 @@ client.on("message", message => {
 		return message.channel.send(bot_embed);
 	} else if(message.content === "r!roast"){
         const random_roasts = Math.ceil(Math.random() * 54);
-		return message.channel.send(roasts[random_roasts - 1].roast + `\n **Meme #${random}** <:roast_circle:474755210485563404>`);
+		return message.channel.send(roasts[random_roasts - 1].roast + `\n **Meme #${random_roasts}** <:roast_circle:474755210485563404>`);
     } else if(message.content.startsWith("r!roast ")){
 		const random = Math.ceil(Math.random() * 54);
 
