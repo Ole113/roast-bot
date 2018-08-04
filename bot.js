178941d4d3af30f2295fb99764733c586e70eee9
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-03 21:23:03 -0600

    r!meme specific roast now live

--- bot.js
+++ bot.js
@@ -161,7 +161,7 @@ client.on("message", message => {
 		const random_memes = Math.ceil(Math.random() * 107);
 		if(message.content.startsWith("r!meme #")){
 			let word2 = message.content;
-			let number2 = word2.slice(9, word2.length);
+			let number2 = word2.slice(8, word2.length);
 			let number_int1 = parseInt(number2);
 			return message.channel.send(`Meme #${number_int1 - 1} <:roast_circle:474755210485563404>`, {files: [`Images/meme${number1_int}.PNG`]})
 		}
