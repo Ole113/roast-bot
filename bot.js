1f1905013aaff0a6d7888a52ff91b95d4fa251ba
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-06 19:36:31 -0600

    fix

--- bot.js
+++ bot.js
@@ -116,27 +116,7 @@ client.on("message", message => {
 		}
 		client.points.setProp (key, "level", curLevel);
 	}
-	if(message.content === "r!leaderboard") {
-		// Get a filtered list (for this guild only), and convert to an array while we're at it.
-		const filtered = client.points.filterArray( p => p.guild === message.guild.id );
-	  ​
-		// Sort it to get the top results... well... at the top. Y'know.
-		const sorted = filtered.sort((a, b) => a.points < b.points);
-	  ​
-		// Slice it, dice it, get the top 10 of it!
-		const top10 = sorted.splice(0, 10);
-	  ​
-		// Now shake it and show it! (as a nice embed, too!)
-		const embed = new Discord.RichEmbed()
-		  .setTitle("Leaderboard")
-		  .setAuthor(client.user.username, client.user.avatarURL)
-		  .setDescription("Our top 10 points leaders!")
-		  .setColor(0x00AE86);
-		for(const data of top10) {
-		  embed.addField(client.users.get(data.user).tag, `${data.points} points (level ${data.level})`);
-		}
-		return message.channel.send({embed});
-	  }
+
 	if (message.content === "r!points") {
 		return message.channel.send(`You currently have ${client.points.getProp(key, "points")}, and are level ${client.points.getProp(key, "level")}!`);
 	  }
