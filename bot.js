f6b8e03b099dc69af6f1fe99e0adff37835ff4fc
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-07 09:14:24 -0600

    fix

--- bot.js
+++ bot.js
@@ -117,11 +117,11 @@ client.on("message", message => {
 
 		client.points.set(key, curLevel, "level");
 	}
-	const key = `${message.guild.id}-${message.author.id}`;
+	/*const key = `${message.guild.id}-${message.author.id}`;
 	if(client.points.get(key, "points") == 5){
 
 		return message.reply("Your now level 2");
-	}
+	} */
 	if (message.content === "r!level") {
 		const key = `${message.guild.id}-${message.author.id}`;
 		return message.channel.send(`You currently have ${client.points.get(key, "points")} points , and are level ${client.points.get(key, "level")}!`);
