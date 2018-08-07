74c9f39ff8e2c578acf8b1bfd5a1726f1d3c04d3
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-07 09:57:31 -0600

    fix

--- bot.js
+++ bot.js
@@ -110,7 +110,7 @@ client.on("message", message => {
 		}
 		let currentPoints = client.points.get(key, "points");
 		client.points.set(key, ++currentPoints, "points");
-		const curLevel = 10;
+		const curLevel = 0.1;
 		if (client.points.get(key, "level") < curLevel) {
 			return message.reply(`You've leveled up to level **${curLevel}**! Congrats`);
 		}
