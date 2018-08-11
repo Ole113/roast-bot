ef3fd4c4507f3e8c2b0bac48727c2a32b97d96a4
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-10 22:23:14 -0600

    level command taken out :(

--- bot.js
+++ bot.js
@@ -20,60 +20,60 @@ client.points = new Enmap({provider: new Provider({name: "points"})});
 *
 */
 const roasts = [
-	{"roast":"Id offer you some gum but your smiles got plenty of it"},
-	{"roast":"Repeat After me: semen is not hair gel"},
-	{"roast":"Your body fat is about as evenly distributed as wealth in the US economy"},
-	{"roast":"Your like dobby from harry potter, only people wont be sad when you die in the seventh book"},
-	{"roast":"If I asked you about your cock it wouldnt be a very long conversation"},
-	{"roast":"You sound like a founder of a start up company that makes male tampons"},
-	{"roast":"You have the kinds of looks that make people talk about your personality"},
-	{"roast":"You look like what happens when you press random on the charecter creation menu"},
-	{"roast":"You look like the after picture of a meth ad"},
-	{"roast":"Even the shower doesnt want to see you naked"},
-	{"roast":"I bet you wear a nose ring because no one wants to put one on your finger"},
-	{"roast":"You must hear \"lets be friends often\" I mean \"lets be cousins\" alot"},
-	{"roast":"When the airforce needs extra landing space they should just rent out your forehead"},
-	{"roast":"If laughter is the best medicine, your face must be curing the world"},
-	{"roast":"If I wanted a bitch Id have bought a dog"},
-	{"roast":"The only way you will ever get laid is if you crawl up a chickens ass and wait"},
-	{"roast":"It looks like your face caught fire and someone tried to put it out with a hammer"},
-	{"roast":"Your family tree must be a cactus because everyone on it is a prick"},
-	{"roast":"Save your breath - youre going to need it to blow up your date"},
-	{"roast":"Your proof evolution can go in reverse"},
+	{"roast":"Id offer you some gum but your smiles got plenty of it."},
+	{"roast":"Repeat After me: semen is not hair gel."},
+	{"roast":"Your body fat is about as evenly distributed as wealth in the US economy."},
+	{"roast":"Your like dobby from harry potter, only people wont be sad when you die in the seventh book."},
+	{"roast":"If I asked you about your cock it wouldnt be a very long conversation."},
+	{"roast":"You sound like a founder of a start up company that makes male tampons."},
+	{"roast":"You have the kinds of looks that make people talk about your personality."},
+	{"roast":"You look like what happens when you press random on the charecter creation menu."},
+	{"roast":"You look like the after picture of a meth ad."},
+	{"roast":"Even the shower doesnt want to see you naked."},
+	{"roast":"I bet you wear a nose ring because no one wants to put one on your finger."},
+	{"roast":"You must hear \"lets be friends often\" I mean \"lets be cousins\" alot."},
+	{"roast":"When the airforce needs extra landing space they should just rent out your forehead."},
+	{"roast":"If laughter is the best medicine, your face must be curing the world."},
+	{"roast":"If I wanted a bitch Id have bought a dog."},
+	{"roast":"The only way you will ever get laid is if you crawl up a chickens ass and wait."},
+	{"roast":"It looks like your face caught fire and someone tried to put it out with a hammer."},
+	{"roast":"Your family tree must be a cactus because everyone on it is a prick."},
+	{"roast":"Save your breath - youre going to need it to blow up your date."},
+	{"roast":"Your proof evolution can go in reverse."},
 	{"roast":"When you were born, the doctor came out to the waiting room and said to your dad, \"I'm very sorry. We did everything we could. But he pulled through.\""},
-	{"roast":"You've got less meat in your pants than there is in a vegetarian restaurant"},
-	{"roast":"I wasn't born with enough middle fingers to let you know how I feel about you"},
-	{"roast":"Your birth certificate is an apology letter from the condom factory"},
-	{"roast":"You're about as useful as a vibrator with no batteries"},
+	{"roast":"You've got less meat in your pants than there is in a vegetarian restaurant."},
+	{"roast":"I wasn't born with enough middle fingers to let you know how I feel about you."},
+	{"roast":"Your birth certificate is an apology letter from the condom factory."},
+	{"roast":"You're about as useful as a vibrator with no batteries."},
 	{"roast":"Fake hair, fake nails, fake smile. Are you sure you weren't made in China?"},
-	{"roast":"Mirrors can't talk, and lucky for you they can't laugh either"},
-	{"roast":"I'd say you're funny, but looks aren't everything"},
-	{"roast":"You must have been born on a highway because thats where most accidents happen"},
-	{"roast":"When I see your face theres not a thing I would change... Except for the direction im walking in"},
-	{"roast":"Your so ugly when you popped out the doctor said aww what a treasure and your mom said yeah lets bury it"},
-	{"roast":"I hear when you were a child your mother wanted to hire somebody to take care of you, but the mafia wanted too much"},
-	{"roast":"You're so fat the only letters of the alphabet you know are KFC"},
-	{"roast":"The only positive thing about you is your HIV status"},
-	{"roast":"You're like STDs, nobody wants you, everyone hates you and it proves your parents should have used protection"},
-	{"roast":"The only way I'd lay naked with you would be in a mass grave"},
-	{"roast":"You're the cum your mother should have swallowed"},
-	{"roast":"I heard your mom got fired from her job at the sperm bank - the boss caught her drinking on the job"},
+	{"roast":"Mirrors can't talk, and lucky for you they can't laugh either."},
+	{"roast":"I'd say you're funny, but looks aren't everything."},
+	{"roast":"You must have been born on a highway because thats where most accidents happen."},
+	{"roast":"When I see your face theres not a thing I would change... Except for the direction im walking in."},
+	{"roast":"Your so ugly when you popped out the doctor said aww what a treasure and your mom said yeah lets bury it."},
+	{"roast":"I hear when you were a child your mother wanted to hire somebody to take care of you, but the mafia wanted too much."},
+	{"roast":"You're so fat the only letters of the alphabet you know are KFC."},
+	{"roast":"The only positive thing about you is your HIV status."},
+	{"roast":"You're like STDs, nobody wants you, everyone hates you and it proves your parents should have used protection."},
+	{"roast":"The only way I'd lay naked with you would be in a mass grave."},
+	{"roast":"You're the cum your mother should have swallowed."},
+	{"roast":"I heard your mom got fired from her job at the sperm bank - the boss caught her drinking on the job."},
 	{"roast":"You should wear a condom on your head because if you're gonna act like a dick you might as well dress like one!"},
-	{"roast":"Twinkle twinkle little star, I want to hit you with my car, Throw you off a cliff so high, I hope you break your neck and die"},
-	{"roast":"Some babies were dropped on their heads but you were clearly thrown at a wall"},
-	{"roast":"Roses are red, shit is brown, shut the fuck up, and sit the fuck down"},
-	{"roast":"Fashion Tip 101: You only need to wear one pair of socks at a time and they belong on your feet not in your bra"},
-	{"roast":"I see you were so impressed with your first chin that you added two more"},
-	{"roast":"Twinkle twinkle little whore, close your legs, they're not a door"},
+	{"roast":"Twinkle twinkle little star, I want to hit you with my car, Throw you off a cliff so high, I hope you break your neck and die."},
+	{"roast":"Some babies were dropped on their heads but you were clearly thrown at a wall."},
+	{"roast":"Roses are red, shit is brown, shut the fuck up, and sit the fuck down."},
+	{"roast":"Fashion Tip 101: You only need to wear one pair of socks at a time and they belong on your feet not in your bra."},
+	{"roast":"I see you were so impressed with your first chin that you added two more."},
+	{"roast":"Twinkle twinkle little whore, close your legs, they're not a door."},
 	{"roast":"I guess those penis enlargement pills are working - you're twice the dick you were yesterday!"},
-	{"roast":"Twinkle Twinkle little slut, name a guy you haven't fucked, was he skinny, was he tall, Nevermind you did them all"},
-	{"roast":"I failed a spelling test because they asked me how to spell 'bitch' and I wrote down your name"},
-	{"roast":"You're like a light switch, even a little kid can turn you on"},
-	{"roast":"I don't see any penises in the general vicinity... So I'm wondering why you keep opening your fucking mouth"},
+	{"roast":"Twinkle Twinkle little slut, name a guy you haven't fucked, was he skinny, was he tall, Nevermind you did them all."},
+	{"roast":"I failed a spelling test because they asked me how to spell 'bitch' and I wrote down your name."},
+	{"roast":"You're like a light switch, even a little kid can turn you on."},
+	{"roast":"I don't see any penises in the general vicinity... So I'm wondering why you keep opening your fucking mouth."},
 	{"roast":"Who lit the fuse on your tampon?"},
-	{"roast":"I thought bra's are meant for boobs not tissues"},
-	{"roast":"Twinkle twinkle little slut, You like dick inside your butt"},
-	{"roast":"The last time I saw a face like yours I fed it a banana"},
+	{"roast":"I thought bra's are meant for boobs not tissues."},
+	{"roast":"Twinkle twinkle little slut, You like dick inside your butt."},
+	{"roast":"The last time I saw a face like yours I fed it a banana."},
 	{"roast":"Roses are red, violets are blue. I have five fingers and the middle one is for you."},
 	{"roast": "I'd like to kick you in the teeth but why improve your looks?"},
 	{"roast": "At least there's one good thing about your body. It isn't as ugly as your face."},
@@ -137,9 +137,9 @@ const roasts = [
 *
 */
 client.on("ready", () => {
-	console.log("-------------------------------------------------")
 	console.log("Roast-Bot is Ready");
 	console.log("Number of servers Roast-Bot is in: " + client.guilds.size);
+	console.log("-------------------------------------------------")
     client.user.setActivity("r!help", { type: "PLAYING" })	   
 });
 /*
@@ -183,15 +183,17 @@ client.on("guildMemberRemove", member => {
 *
 */
 client.on("guildCreate", guild => {
-	console.log("-------------------------------------------------")
 	console.log("Roast Bot joined a new server named: " + guild.name);
-	console.log(`# of people in ${guild.name} is ${guild.memberCount} people`);
+	console.log(`# of people in ${guild.name} is ${guild.memberCount} people.`);
 	console.log("-------------------------------------------------")
 });
 client.on("guildDelete", guild => {
 	console.log("Roast-Bot left a server named: " + guild.name);
+	console.log(`# of people in ${guild.name} is ${guild.memberCount} people.`)
+	console.log("-------------------------------------------------")
 })
 client.on("message", message => {
+	
 	/*
 	*
 	*   Things to add to Database:
@@ -202,7 +204,7 @@ client.on("message", message => {
 	*  Custom Meme adding.
 	*  When you level up stil send the command that they did but then do the level up thing.
 	*  Make it so people cannot spam Roast-Bot commands to get more XP.
-	*/
+	*
 	if(message.author.bot) return;
 	if(message.content.startsWith("r!")) {
 		const key = `${message.guild.id}-${message.author.id}`;
@@ -244,18 +246,23 @@ client.on("message", message => {
 		}
 		client.points.set(key, curLevel, "level");
 	}
-	/*
+	
 	*
 	*   Things to add to r!level:
 	*  ---------------------------
 	*  
 	*
-	*/
+	
 	if (message.content === "r!level") {
 		const key = `${message.guild.id}-${message.author.id}`;
 		let currentPoints = client.points.get(key, "points");
 		client.points.set(key, --currentPoints, "points");
 		return message.reply(` You currently have ${client.points.get(key, "points")} XP, and are level ${client.points.get(key, "level")}! <:roast_circle:474755210485563404>`);
+	
+	}
+	*/
+
+
 	/*
 	*
 	*   Things to add to r!help:
@@ -263,7 +270,7 @@ client.on("message", message => {
 	*  
 	*
 	*/
-	} else if(message.content === "r!help") {
+	if(message.content === "r!help") {
 		let help_icon = client.user.displayAvatarURL;
 		let help_embed = new Discord.RichEmbed()
 		.setColor("#EB671D")
