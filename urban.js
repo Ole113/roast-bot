451fdaac0df0f852fb9b6d75b6fb3c6347ca314c
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-11 15:00:24 -0600

    urban dictionary

--- urban.js
+++ urban.js
@@ -1,10 +1,24 @@
 const Discord = require("discord.js");
-const urban = require("urban");
-module.exports.run = async(bot, message, args) => {
-    if(args.length < 1) return message.channel.send("Please enter what you want to search up.")
-    console.log(args.join(" "));
-}
+const urban = require("relevant-urban");
 
-module.exports.help = {
-    name: "urban"
+exports.run = async(client, message, args, tools) => {
+    if(!args[0]) return message.channel.send("Please specify what you want to be searched.");
+
+    let res = await urban(args.join(" ")).catch(e => {
+        return message.channel.send("Sorry, that word wasn't found.");
+    })
+
+    const embed = new Discord.MessageEmbed()
+    .setColor("#EB671D")
+    .setTitle(res.word)
+    .setURL(res.urbanURL)
+    .setDescription(`**Definition:**\n *${res.definition}*\n\n**Example:**\n*${res.example}*`)
+    .addField("Author", res.author, true)
+    .addField("Rating", `**\`Upvotes: ${res.thumbsUp} | Downvotes: ${res.thumbsDown}\`**`)
+
+    if(res.tags.length > 0 && res.tags.join(" ").length < 1024) {
+        embed.addField("Tags", res.tags.join(", "), true)
+    }
+
+    message.channel.send(embed);
 }
