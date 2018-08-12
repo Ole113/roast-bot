e30d036c976c93ae57a1ca1d9abab8605c03e01c
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-12 17:49:32 -0600

    r!help update

--- bot.js
+++ bot.js
@@ -287,7 +287,7 @@ client.on("message", message => {
 		.addField("r!clear NUMBER", "Choose how many messages you want to delete. Max is 100. **To use this command Roast-Bot needs to given Manage Messages permissions.**")
 		.addField("r!say", "To use this command use `r!say ` and then what you want Roast-Bot to say.")
 		.addBlankField()
-		.addField("Utilities:", "")
+		.addField("Utilities:", " ")
 		.addField("welcome-leave-log", "To use the Roast-Bot welcome-leave-log make a channel named \"welcome-leave-log\". If you don't want to use the log just don't make a channel named welcome-leave-log.")
 		//.addField("XP-System", "Everytime you use a Roast-Bot command your XP increases! Use r!level to check your level and XP! **r!level is in BETA** Level 1: 0-9XP, Level 2: 10XP, Level 3: 15XP, Level 4: 25XP Level 5: 50XP, Level 6: 100XP, Level 7: 200XP, Level 8: 500XP, Level 9: 1,000XP, Level 10: 10,000XP")
 		.addBlankField()
