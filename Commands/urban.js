e9bbacedd18a525687bd396a73598cdf8c6e3954
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-13 14:08:45 -0600

    urban test update

--- Commands/urban.js
+++ Commands/urban.js
@@ -1,3 +1,22 @@
-exports.run = async(client, message) => {
-     message.channel.send("test complete");
+const urban = require("relevant-urban");
+const Discord = require("discord.js");
+
+exports.run = async(client, message, args, tools) => {
+    let res = await urban(args.join(" ")).catch(e => {
+        return message.channel.send("**Sorry that word was not found :(**");
+    });
+
+    const urban_embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle(res.word)
+    .setURL(res.urbanURL)
+    .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n${res.example}`)
+    .addField("Author:", res.author, true)
+    .addField("Rating:", `**Upvotes:** ${res.thumbsUp} | **Downvotes:** ${res.thumbsDown}`)
+
+    if(res.tags.length > 0 && res.tags.join(" ").length < 1024) {
+        urban_embed.addField("Tags", res.tags.join(", "), true)
+    }
+
+    return message.channel.send(urban_embed);
 }
