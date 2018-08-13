b51f7dffc156806409451737dbd7cc1bb3d58fc4
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-13 15:13:51 -0600

    fix

--- Commands/urban.js
+++ Commands/urban.js
@@ -3,15 +3,15 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 exports.run = async(client, message, args) => {
-    let res = await urban(args).catch(e => {
-        return message.channel.send("**Word not found :( <:roast_circle:474755210485563404>**")
-    });
+    let res = await urban(args);
+    if(!res) return message.channel.send("**Word not found :(  <:roast_circle:474755210485563404>**")
+
 
     const urban_embed = new Discord.RichEmbed()
         .setColor("#EB671D")
         .setTitle(res.word)
         .setURL(res.urbanURL)
-        .setDescription(`\n\n**Definition:**\n*${res.definition}*\n\n**Example:**\n${res.example}`)
+        .setDescription(`Definition:\n*${res.definition}*\n\n**Example:**\n${res.example}`)
         .addField("Author:", res.author, true)
         .addField("Rating:", `**Upvotes: :thumbsup:** ${res.thumbsUp} | **Downvotes: :thumbsdown:** ${res.thumbsDown}`);
 
