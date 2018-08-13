9fcbd4f507dedb2e7f0bc7cd02da19a00ca3e7da
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-13 14:45:14 -0600

    test update

--- Commands/urban.js
+++ Commands/urban.js
@@ -3,19 +3,17 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 exports.run = async(client, message, args) => {
-    let res = urban(args).then(variableName => {
-        //do whatever with variableName
-    }).catch(e => {
-        return mesage.channel.send("**Word not found :(");
+    let res = await urban(args.join(' ')).catch(e => {
+        return mesage.channel.send("**Word not found**")
     });
 
     const urban_embed = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setTitle(res.word)
-    .setURL(res.urbanURL)
-    .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n${res.example}`)
-    .addField("Author:", res.author, true)
-    .addField("Rating:", `**Upvotes:** ${res.thumbsUp} | **Downvotes:** ${res.thumbsDown}`)
+        .setColor("#EB671D")
+        .setTitle(res.word)
+        .setURL(res.urbanURL)
+        .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n${res.example}`)
+        .addField("Author:", res.author, true)
+        .addField("Rating:", `**Upvotes:** ${res.thumbsUp} | **Downvotes:** ${res.thumbsDown}`)
 
     /*if(res.tags.length > 0 && res.tags.length < 1024) {
         urban_embed.addField("Tags", res.tags.join(", "), true)
