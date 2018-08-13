aa815d26f7cf064434ade6dabe3b141df6233017
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-12 22:58:40 -0600

    Command Folder added

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 
-const urban_file = require("./urban.js");
+const urban_file = require("./Commands/urban.js");
 //const Enmap = require("enmap");
 //const Provider = require("enmap-sqlite");
 //client.points = new Enmap({provider: new Provider({name: "points"})});
