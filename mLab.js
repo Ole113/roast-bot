8e42c273a3170db6f8df8bccd16f089fb4597365
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-14 13:29:53 -0600

    db update

--- mLab.js
+++ mLab.js
@@ -9,5 +9,29 @@
 *  Make it so people cannot spam Roast-Bot commands to get more XP.
 *
 */
-const mLabKey_file = require("mLabKey.json");
-const mLab = require("mongolab-data-api")(API_KEY);
+const mLabHidden_file = require("mLabKey.json");
+const MongoClient = require('mongodb').MongoClient;
+
+const MONGO_URL = URL;
+
+
+MongoClient.connect(MONGO_URL, (err, db) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    // Do something with db here, like inserting a record
+    db.collection('notes').insertOne(
+        {
+            title: 'Hello MongoDB',
+            text: 'Hopefully this works!'
+        },
+        function (err, res) {
+            if (err) {
+                db.close();
+                return console.log(err);
+            }
+            db.close();
+        }
+    )
+});
