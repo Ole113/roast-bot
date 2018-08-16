acb2f542c6f51f59affc04a32c7d5b71a3bf00bd
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-15 21:31:37 -0600

    db fix

--- mLab.js
+++ mLab.js
@@ -11,7 +11,7 @@
 */
 const Discord = require("discord.js")
 const client = new Discord.Client();
-const mLabHidden_file = require("mLabHidden.json");
+const mLabHidden_file = require("./mLabHidden.json");
 var mLab = require('mongolab-data-api')(API_KEY);
 
 exports.run = async(message) => {
