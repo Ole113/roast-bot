ffb01e45eabef196eca9412ebb1698303990d4bd
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-15 22:03:08 -0600

    tset

--- mLab.js
+++ mLab.js
@@ -11,59 +11,58 @@
 */
 const Discord = require("discord.js")
 const client = new Discord.Client();
-//const mLabHidden_file = require("./mLabHidden.json");
-var mLab = require('mongolab-data-api')("mibGkayB-6avS1d89zaKFVHCLEZB5i8O");
-
-exports.run = async() => {
-client.on('message', message => {
-    if (message.content.startsWith("r!")) {
-        var options = {
-            database: 'roast-bot-ole113',
-            collectionName: member.guild.id,
-            query: `{"id": ${message.member.id} }`
-        };
-        mLab.listDocuments(options, function (err, data) { member = data });
-        member.exp += 1;
-        options = {
-            database: 'roast-bot-ole113',
-            collectionName: member.guild.id,
-            query: `{"id": ${message.member.id} }`,
-            document: member
-        };
-        mlab.updateDocuments(options, function () {
-            console.log(`${message.member.nickname} gained 25 exp`)
-        });
-    }
-});
-client.on("guildCreate", guild => {
-    var options = {
-        database: "roast-bot-ole113",
-        collectionName: guild.id,
-        documents: []
-    };
-    guild.members.forEach(function (member) {
-        options.documents.push({
-            id: member.id,
-            exp: 0
-        });
+const test = API_TOKEN;
+const mLabHidden_file = require("./mLabHidden.json");
+var mLab = require('mongolab-data-api')(test);
+//mibGkayB-6avS1d89zaKFVHCLEZB5i8O
+exports.run = async () => {
+    client.on('message', message => {
+        if (message.content.startsWith("r!")) {
+            var options = {
+                database: 'roast-bot-ole113',
+                collectionName: member.guild.id,
+                query: `{"id": ${message.member.id} }`
+            };
+            mLab.listDocuments(options, function (err, data) { member = data });
+            member.exp += 1;
+            options = {
+                database: 'roast-bot-ole113',
+                collectionName: member.guild.id,
+                query: `{"id": ${message.member.id} }`,
+                document: member
+            };
+            mlab.updateDocuments(options, function () {
+                console.log(`${message.member.nickname} gained 25 exp`)
+            });
+        }
     });
-    options.documents.push({
-        id: guild.id,
-        queue: []
-    });
-    options.documents.push()
-    mLab.insertDocuments(options)
-    console.log("Joined a server")
-});
-client.on("guildDelete", guild => {
-    guild.members.forEach(function (member) {
+    client.on("guildCreate", guild => {
         var options = {
             database: "roast-bot-ole113",
-            collectionName: guild.id
-        }
-        mLab.deleteDocuments(options, function (err, data) {
-            console.log("Left a server");
+            collectionName: guild.id,
+            documents: []
+        };
+        guild.members.forEach(function (member) {
+            options.documents.push({
+                id: member.id,
+                exp: 0
+            });
+        });
+        options.documents.push({
+            id: guild.id,
+            queue: []
+        });
+        options.documents.push()
+        mLab.insertDocuments(options)
+    });
+    client.on("guildDelete", guild => {
+        guild.members.forEach(function (member) {
+            var options = {
+                database: "roast-bot-ole113",
+                collectionName: guild.id
+            }
+            mLab.deleteDocuments(options, function (err, data) {
+            });
         });
     });
-});
 }
