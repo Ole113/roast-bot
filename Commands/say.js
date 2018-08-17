762587e582d1a123be24a4791ef785e9d9e53102
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-17 12:17:15 -0600

    Update say.js

--- Commands/say.js
+++ Commands/say.js
@@ -10,7 +10,7 @@ const Discord = require("discord.js");
 exports.run = async (message) => {
     if (message.author.bot) return;
     if(message.content == "r!say" || message.content == "r!say  "){
-        return message.channel.send("Please provide what you want Roast-Bot to say.");
+        return message.channel.send("Please provide what you want Roast-Bot to say. The correct usage is `r!say whatToSay`. ");
     }
     if (message.content.startsWith("r!say ")) {
         const word = message.content;
