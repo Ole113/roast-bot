281f47c4bc3d5b3a9451e4d63aa8eec8aa467226
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-17 12:14:18 -0600

    Update say.js

--- Commands/say.js
+++ Commands/say.js
@@ -9,12 +9,12 @@ const Discord = require("discord.js");
 
 exports.run = async (message) => {
     if (message.author.bot) return;
+    if(message.content == "r!say" || message.content == "r!say  "){
+        return message.channel.send("Please provide what you want Roast-Bot to say.");
+    }
     if (message.content.startsWith("r!say ")) {
         const word = message.content;
         const say = word.slice(6, word.length);
-        if(say === " " || say === ""){
-           return message.channel.send("Please provide what you want Roast-Bot to say.");
-        }
         return message.channel.send(say);
     }
 }
