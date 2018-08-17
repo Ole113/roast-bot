3664ac454610421fa9cca6132553a7687e5e76c7
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-17 12:00:47 -0600

    Update say.js

--- Commands/say.js
+++ Commands/say.js
@@ -12,7 +12,7 @@ exports.run = async (message) => {
     if (message.content.startsWith("r!say ")) {
         const word = message.content;
         const say = word.slice(6, word.length);
-        if(say == " "){
+        if(say === " " || say === ""){
            return message.channel.send("Please provide what you want Roast-Bot to say.");
         }
         return message.channel.send(say);
