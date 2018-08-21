f99a8e2887065cfdb61cf68acb5ccb9f9abeec91
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-21 12:10:15 -0600

    test

--- XP-System/xpAdd.js
+++ XP-System/xpAdd.js
@@ -7,7 +7,7 @@
 const Discord = require("discord.js");
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/XP-System");
+mongoose.connect("mongodb://localhost/XP-System");
 
 //const mongodb = require("mongodb");
 //var db = connect("localhost:27017/XP-System");
