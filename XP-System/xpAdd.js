93f9eab32633e8cf48c28840ac46b16da98019cc
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-20 22:34:03 -0600

    test

--- XP-System/xpAdd.js
+++ XP-System/xpAdd.js
@@ -9,6 +9,8 @@ const Discord = require("discord.js");
 const mongoose = require("mongoose");
 //mongoose.connect("mongodb://roast-bot-ole113-npqi1.gcp.mongodb.net:27017/XP-System");
 //mongoose.connect("mongodb://127.0.0.1:27017/XP-System");
+var opts = { server: {auto_reconnect: false }, user: "Admin", pass: "aelb2580"}
+db = mongoose.createConnection("localhost", "XP-System", 27017, opts)
 mongoose.connect('mongodb://Admin:aelb2580@localhost:27017/XP-System');
 exports.run = async () => {
 
