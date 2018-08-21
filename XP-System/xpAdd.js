b5a8e816c05d5eb2beb0c5bdfee1e29d17899cb7
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-20 19:01:06 -0600

    db fix

--- XP-System/xpAdd.js
+++ XP-System/xpAdd.js
@@ -7,7 +7,7 @@
 const Discord = require("discord.js");
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/XP-System");
+mongoose.connect("mongodb://roast-bot-ole113-npqi1.gcp.mongodb.net:27017/XP-System");
 
 exports.run = async () => {
 
