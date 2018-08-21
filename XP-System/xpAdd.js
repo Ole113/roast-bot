44b901f17842f896315dc9b3f89130dc62a7e26c
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-20 20:48:28 -0600

    test

--- XP-System/xpAdd.js
+++ XP-System/xpAdd.js
@@ -7,8 +7,8 @@
 const Discord = require("discord.js");
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://roast-bot-ole113-npqi1.gcp.mongodb.net:27017/XP-System");
-
+//mongoose.connect("mongodb://roast-bot-ole113-npqi1.gcp.mongodb.net:27017/XP-System");
+mongoose.connect("mongodb://localhost:27017/XP-System");
 exports.run = async () => {
 
 }
