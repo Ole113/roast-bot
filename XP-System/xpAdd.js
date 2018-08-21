4ebff810e223d98c7cac34db51d6437e9227d339
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-21 12:03:17 -0600

    fix

--- XP-System/xpAdd.js
+++ XP-System/xpAdd.js
@@ -7,10 +7,10 @@
 const Discord = require("discord.js");
 
 const mongoose = require("mongoose");
-//mongoose.connect("mongodb://localhost:27017/XP-System");
+mongoose.connect("mongodb://localhost:27017/XP-System");
 
-const mongodb = require("mongodb");
-var db = connect("localhost:27017/XP-System");
+//const mongodb = require("mongodb");
+//var db = connect("localhost:27017/XP-System");
 exports.run = async () => {
 
 }
