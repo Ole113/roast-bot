8a902df4ee14d8597e1f9919e0f6323a2fe3c355
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-24 20:20:04 -0600

    r!server help added

--- Commands/bot.js
+++ Commands/bot.js
@@ -10,6 +10,9 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 exports.run = async (client, message) => {
+    if(message.content == "r!bot help"){
+        return message.channel.send("comming soon")
+    }
     if (message.content == "r!bot") {
         let bot_icon = client.user.displayAvatarURL;
         let bot_embed = new Discord.RichEmbed()
--- Commands/server.js
+++ Commands/server.js
@@ -9,6 +9,9 @@
 const Discord = require("discord.js");
 
 exports.run = async (message) => {
+    if(message.content == "r!server help"){
+        return message.channel.send("**r!server help:**\n\n`r!server` tells you information about your server. The information that it provides is: Number of people in server, Server name, the date you joined, and the date the server was created.\n\n*Example:\n\n*USER: r!server\nRoast-Bot: Server Name....\nDate the server was created....\nDate you joined....\nNumber of people in the server....\n\n`r!server` will still work even if you put 1 or more space after the “server”.  It will not work however if you put 1 or more spaces and then a character that isn’t a space.")
+    }
     if (message.content == "r!server") {
         let server_icon = message.guild.iconURL;
         let server_embed = new Discord.RichEmbed()
