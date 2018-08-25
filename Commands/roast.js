8ec801f3c35f2743f8355598f90d2c470c65761f
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-25 11:51:13 -0600

    test

--- Commands/roast.js
+++ Commands/roast.js
@@ -116,7 +116,7 @@ exports.run = async (message) => {
 	if (message.content.startsWith("r!roast")) {
 		if (message.content === "r!roast") {
 			const random_roasts = Math.ceil(Math.random() * 101);
-			return message.channel.send(roasts[random_roasts - 1].roast + `\n **Roast #${random_roasts}** <:roast_circle:474755210485563404>`);
+			return message.channel.send(roasts[random_roasts].roast + `\n **Roast #${random_roasts}** <:roast_circle:474755210485563404>`);
 		} else if (message.content.startsWith("r!roast ")) {
 			const random = Math.ceil(Math.random() * 101);
 			const word = message.content;
@@ -128,7 +128,7 @@ exports.run = async (message) => {
 				return message.channel.send(roasts[number_int].roast + `\n **Roast #${number_int}** <:roast_circle:474755210485563404>`);
 			}
 
-			return message.channel.send(reply + ", " + roasts[random - 1].roast + `\n **Roast #${random}** <:roast_circle:474755210485563404>`);
+			return message.channel.send(reply + ", " + roasts[random].roast + `\n **Roast #${random}** <:roast_circle:474755210485563404>`);
 		}
 	}
 }
