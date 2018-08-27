dbe3bc6eff9cd2c94e38e5742e21b19399e9cba5
Ole113 <alex_elbel@icloud.com>
2018-08-27 15:41:01 -0600

    Update invite.js

--- Commands/invite.js
+++ Commands/invite.js
@@ -14,7 +14,7 @@ exports.run = async (message) => {
 if(message.content == "r!invite") {
     let invite_embed = new Discord.RichEmbed()
         .setColor("#EB671D")
-        .addField("Invite Link", "<:roast_circle:474755210485563404>")
+        .setDescription("Invite Link  <:roast_circle:474755210485563404>")
         .setURL("https://discordapp.com/oauth2/authorize?client_id=461361233644355595&scope=bot&permissions=8")
         .setFooter("Roast-Bot v1.9.0");
     return message.channel.send(invite_embed);
