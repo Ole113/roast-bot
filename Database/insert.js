df3dca8ef66a1fdfbd39519a4be15ecaac970067
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-31 15:27:36 -0600

    db test

---
+++ Database/insert.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "Russ",
+    password: "Dbossman1",
+    database: "xp_system"
+})
+database.connect();
