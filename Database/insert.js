fe7cbce6c586d9ca23c3a7648f6540aa585e7c76
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-31 18:00:22 -0600

    update

--- Database/insert.js
+++ Database/insert.js
@@ -4,9 +4,8 @@ const mysql = require("mysql");
 exports.run = async () => {
 var connection = mysql.createConnection({
     host: "localhost",
-    user: "Russ",
-    password: "Dbossman1",
-    database: "xp_system"
+    user: "Admin",
+    password: "aelb2580",
 })
 database.connect();
 }
