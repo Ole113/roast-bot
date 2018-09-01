7ef31a741ae5234ce7d773a9979dd815caad9e67
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-31 21:04:12 -0600

    update

--- Database/insert.js
+++ Database/insert.js
@@ -5,11 +5,12 @@ exports.run = async () => {
 
 try{
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
+    port: "3306",
     user: "admin",
     password: "Vector$bestGGun1158",
     database: "roast-bot-db"
-})
+});
 connection.connect();
 }
 catch(error){
