0da5e1c6391c5eb5bf2e2a99eed710b799ae5d88
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-08-31 20:54:13 -0600

    update

--- Database/insert.js
+++ Database/insert.js
@@ -9,4 +9,12 @@ var connection = mysql.createConnection({
     database: "roast-bot-db"
 })
 connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
+  
+  connection.end();
+  
 }
