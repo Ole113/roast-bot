5b399ffeb80f489f9f8fc3fac7bcf86b8d8e42e8
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-01 23:12:49 -0600

    update

--- Database/insert.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.use(express.static("public"));
-app.set("view engine", "jade");
-
-app.listen(3000, function() {
-    console.log("Listening on port 3000.")
-})
