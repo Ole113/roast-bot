305205322699e23acfe3e7a432fe8ed8c8f23a6e
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-02 12:44:08 -0600

    db removed

--- Commands/meme.js
+++ Commands/meme.js
@@ -8,12 +8,12 @@
 const Discord = require("discord.js");
 
 exports.run = async (message) => {
+    let word2 = message.content;
+    let number2 = word2.slice(8, word2.length);
+    let number_int1 = parseInt(number2);
     if (message.content.startsWith("r!meme")) {
         const random_memes = Math.ceil(Math.random() * 131);
         if (message.content.startsWith("r!meme #")) {
-            let word2 = message.content;
-            let number2 = word2.slice(8, word2.length);
-            let number_int1 = parseInt(number2);
             if(number_int1 > 131){
                 return message.channel.send("Sorry that meme couldn't be found :(");
             }
