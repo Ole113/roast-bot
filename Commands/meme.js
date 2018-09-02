bc8b3ec1b4d523c7bbfc3f0212e99ba323775c98
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-02 12:52:23 -0600

    update

--- Commands/meme.js
+++ Commands/meme.js
@@ -12,11 +12,11 @@ exports.run = async (message) => {
     let number2 = word2.slice(8, word2.length);
     let number_int1 = parseInt(number2);
     if (message.content.startsWith("r!meme")) {
-        if(number_int1 > 131){
-            return message.channel.send("Sorry that meme couldn't be found :(");
-        }
         const random_memes = Math.ceil(Math.random() * 131);
         if (message.content.startsWith("r!meme #")) {
+            if(number_int1 > 131){
+                return message.channel.send("Sorry that meme couldn't be found :(");
+            }
             return message.channel.send(`Meme #${number_int1} <:roast_circle:474755210485563404>`, { files: [`Images/meme${number_int1}.PNG`] })
         } else {
             return message.channel.send(`Meme #${random_memes} <:roast_circle:474755210485563404>`, { files: [`Images/meme${random_memes}.PNG`] })
