d375f7573aa5996c2524b2e8734984cf0b23c57e
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-08 12:21:53 -0600

    cassandra update

--- Database/main.js
+++ Database/main.js
@@ -6,5 +6,8 @@ const client = new Discord.Client();
 exports.run = async () => {
 console.log("Connected to Cassandra Driver");
 
-//const query = "INSERT INTO roastbotxptable (user_id_xp, user_xp) VALUES(?, ?)";
+const query = "INSERT INTO roastbotxptable (user_id_xp, user_xp) VALUES(?, ?)";
+
+client.execute(query, ["Test_id", "0"])
+.then(result => console.log("Test values added :)", result.rows[0]));
 }
