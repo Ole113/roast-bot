2407c78e1c7a144f8181fa6c149cbdeef4746b0d
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-08 15:06:39 -0600

    update

--- Database/main.js
+++ Database/main.js
@@ -3,14 +3,13 @@ const CasClient = new cassandra.Client({ contactPoints: ["127.0.0.1"], keyspace:
 
 
 exports.run = async () => {
-    try{
     console.log("Connected to Cassandra Driver");
 
     const query = "INSERT INTO roastbotxptable (user_id_xp, user_xp) VALUES(?, ?)";
 
     CasClient.execute(query, ["Test_id", "0"])
-    .then(result => console.log("Test values added :)", result.rows[0]));
-    }catch(error){
+    .then(result => console.log("Test values added :)", result.rows[0])).catch(error => {
         console.log(error);
-    }
+    })
+
 }
