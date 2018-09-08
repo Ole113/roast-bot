91437996f7d77cdc7a606402df7097e9099f664f
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-08 12:23:18 -0600

    mis type fix

--- Database/main.js
+++ Database/main.js
@@ -8,6 +8,6 @@ console.log("Connected to Cassandra Driver");
 
 const query = "INSERT INTO roastbotxptable (user_id_xp, user_xp) VALUES(?, ?)";
 
-client.execute(query, ["Test_id", "0"])
+CasClient.execute(query, ["Test_id", "0"])
 .then(result => console.log("Test values added :)", result.rows[0]));
 }
