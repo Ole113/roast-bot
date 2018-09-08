050505f8d7678303d53b17608abcac50a70ba2e1
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-08 12:28:55 -0600

    test

--- Database/main.js
+++ Database/main.js
@@ -1,12 +1,12 @@
 const cassandra = require("cassandra-driver");
-const CasClient = new cassandra.Client({ contactPoints: ["127.0.0.1:9042"], keyspace: "roastbotxp" });
+const CasClient = new cassandra.Client({ contactPoints: ["127.0.0.1"]});
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 exports.run = async () => {
 console.log("Connected to Cassandra Driver");
 
-const query = "INSERT INTO roastbotxptable (user_id_xp, user_xp) VALUES(?, ?)";
+const query = "INSERT INTO roastbotxp.roastbotxptable (user_id_xp, user_xp) VALUES(?, ?)";
 
 CasClient.execute(query, ["Test_id", "0"])
 .then(result => console.log("Test values added :)", result.rows[0]));
