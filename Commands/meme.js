16a99fa2664f534c38bb47b461ed22f332ce4335
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-10 17:41:45 -0600

    28 more memes added

--- Commands/meme.js
+++ Commands/meme.js
@@ -12,12 +12,12 @@ exports.run = async (message) => {
         return message.channel.send("coming soon")
     }
     if (message.content.toLowerCase().startsWith("r!meme")) {
-        const random_memes = Math.ceil(Math.random() * 131);
+        const random_memes = Math.ceil(Math.random() * 159);
         if (message.content.toLowerCase().startsWith("r!meme #")) {
             let word2 = message.content;
             let number2 = word2.slice(8, word2.length);
             let number_int1 = parseInt(number2);
-            if(number_int1 > 131){
+            if(number_int1 > 159){
                 return message.channel.send("Sorry that meme couldn't be found :(");
             }
             return message.channel.send(`Meme #${number_int1} <:roast_circle:474755210485563404>`, { files: [`Images/meme${number_int1}.PNG`] })
