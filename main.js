8e774131585e32bd33ac89c9fa6b319c2248f876
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-16 19:55:41 -0600

    welcome/leave only for beta bot

--- main.js
+++ main.js
@@ -23,12 +23,40 @@ const meme_file = require("./Commands/meme.js");
 const say_file = require("./Commands/say.js");
 const dbMain_file = require("./Database/main.js");
 
+client.on("guildMemberAdd", member => {
+	let welcomeleavechannel = member.guild.channels.find(c => c.name === "welcome-leave-log");
+	if (!welcomeleavechannel) return;
+	let join_time = new Date();
+	let join_embed = new Discord.RichEmbed()
+	.setTitle(member.user.username + " has joined the server.")
+	.setColor("#EB671D")
+	.addField("Time:", join_time)
+	.addField("Tag:", member)
+	.setThumbnail(member.user.displayAvatarURL);
+	welcomeleavechannel.send(join_embed);
+	console.log(`${member.user.username} has joined the ${member.guild} Discord.`);
+});
+client.on("guildMemberRemove", member => {
+	let welcomeleavechannel = member.guild.channels.find(c => c.name === "welcome-leave-log");
+	let leave_time = new Date();
+	if (!welcomeleavechannel) return;
+	let leave_embed = new Discord.RichEmbed()
+	.setTitle(member.user.username + " has left the server, later aligator.")
+	.setColor("#EB671D")
+	.addField("Time:", leave_time)
+	.addField("Tag:", member)
+	.setThumbnail(member.user.displayAvatarURL);
+	welcomeleavechannel.send(leave_embed);
+	console.log(`${member.user.username} has left the ${member.guild} Discord.`);
+});
+
 client.on("ready", () => {
 	console.log("-----------------------------------")
 	console.log("|      Roast-Bot-Beta is Ready    |");
 	console.log("-----------------------------------")
     client.user.setActivity("rb!help", { type: "PLAYING" })	   
 });
+/* **Archived for main bot**
 client.on("guildCreate", guild => {
 	console.log("✔️Roast Bot joined a new server named: " + guild.name);
 	console.log(`# of people in ${guild.name} is ${guild.memberCount} people.`);
@@ -39,7 +67,7 @@ client.on("guildDelete", guild => {
 	console.log(`# of people in ${guild.name} is ${guild.memberCount} people.`)
 	console.log("-----------------------------------")
 });
-
+*/
 client.on("message", message => {
 	help_file.run(client, message);
 	bot_file.run(client, message);
