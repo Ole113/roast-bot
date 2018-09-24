1e2b5dafdd2cecd2c78111e0972c22c559431fb6
Ole113 <alex_elbel@icloud.com>
2018-09-24 08:59:01 -0600

    Update user.js

--- Commands/user.js
+++ Commands/user.js
@@ -10,18 +10,13 @@ const client = new Discord.Client();
 
 exports.run = async (message) => {
     
-    function status() {
-        if(message.author.equals("online")) {
-            return "Online";
-        }
-    }
     if(message.content.toLowerCase() == "rb!user") {
         let user_embed = new Discord.RichEmbed()
         .setColor("#EB671D")
         .setTitle(`${message.author.username}'s Stats:`)
         .setThumbnail(message.author.displayAvatarURL)
         .addField("Account created at: ", message.author.createdAt.toString())
-        .addField("Current Presense ", status());
+        .addField("Current Presense ", message.author.status);
         //return message.channel.send(message.author.createdAt.toString());
         return message.channel.send(user_embed);
     }
