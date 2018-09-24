09c2f5934fa611ed2dbccfcf6681bde088e7c903
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-24 15:42:06 -0600

    update

--- Commands/user.js
+++ Commands/user.js
@@ -12,20 +12,17 @@ exports.run = async (message) => {
     
     if(message.content.toLowerCase() == "rb!user") {
 
+        var status = "default";
         switch(message.author.presence.status) {
             case "online":
-
+                status = "<:online:493897919641026590> online";
         }
         let user_embed = new Discord.RichEmbed()
         .setColor("#EB671D")
         .setTitle(`${message.author.username}'s Stats:`)
         .setThumbnail(message.author.displayAvatarURL)
         .addField("Account created at: ", message.author.createdAt.toString())
-        .addField("Current Presense ", function() {
-            if(message.author.presence.status == "online") {
-                return "<:online:493897919641026590> online";
-            }
-        });
+        .addField("Current Presense ", status);
         //return message.channel.send(message.author.createdAt.toString());
         return message.channel.send(user_embed);
     }
