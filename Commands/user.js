a199dbfdbd8763aca8e237e2e1243a55e65cb1f7
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-24 15:52:10 -0600

    test

--- Commands/user.js
+++ Commands/user.js
@@ -15,14 +15,21 @@ exports.run = async (message) => {
         var status = "default";
         switch(message.author.presence.status) {
             case "online":
-                status = "<:online:493897919641026590> online";
+                status = "<:online:493897919641026590> <:roast_circle:474755210485563404> Online";
+            case "offline":
+                status = "<:online:493897919641026590> <:roast_circle:474755210485563404>Offline";
+            case "idle":
+                status = "Idle";
+            case "dnd":
+                status = "Do Not Disturb";
         }
         let user_embed = new Discord.RichEmbed()
         .setColor("#EB671D")
         .setTitle(`${message.author.username}'s Stats:`)
         .setThumbnail(message.author.displayAvatarURL)
         .addField("Account created at: ", message.author.createdAt.toString())
-        .addField("Current Presense ", status);
+        .addField("Current Presense:", status);
+
         //return message.channel.send(message.author.createdAt.toString());
         return message.channel.send(user_embed);
     }
