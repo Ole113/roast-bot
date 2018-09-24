caaee6a4f73aa3e9fd9a8ac26476eb8da6035dcb
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-24 15:38:53 -0600

    test

--- Commands/user.js
+++ Commands/user.js
@@ -11,12 +11,21 @@ const client = new Discord.Client();
 exports.run = async (message) => {
     
     if(message.content.toLowerCase() == "rb!user") {
+
+        switch(message.author.presence.status) {
+            case "online":
+
+        }
         let user_embed = new Discord.RichEmbed()
         .setColor("#EB671D")
         .setTitle(`${message.author.username}'s Stats:`)
         .setThumbnail(message.author.displayAvatarURL)
         .addField("Account created at: ", message.author.createdAt.toString())
-        .addField("Current Presense ", message.author.presence.status);
+        .addField("Current Presense ", function() {
+            if(message.author.presence.status == "online") {
+                return "<:online:493897919641026590> online";
+            }
+        });
         //return message.channel.send(message.author.createdAt.toString());
         return message.channel.send(user_embed);
     }
