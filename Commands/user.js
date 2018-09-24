05af01f67fac3502d999d68c3c91dc632c199eeb
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-23 20:58:53 -0600

    update

--- Commands/user.js
+++ Commands/user.js
@@ -10,16 +10,12 @@ const client = new Discord.Client();
 
 exports.run = async (message) => {
     if(message.content.toLowerCase() == "rb!user") {
-
-        if(true == true) {
-            return message.channel.send(client.user.createdAt);
-        } else {
-            return message.channel.send("No Nitro");
-        }
+        return message.channel.send(message.author.createdAt);
+    }
         /*
         let user_embed = new Discord.RichEmbed()
         .setColor("#EB671D")
         .setTitle(message.user);
         */
-    }
+    
 }
