6ceab71352f6184847e12dc26802e6af049191ca
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-23 20:34:26 -0600

    user.js test

---
+++ Commands/user.js
@@ -0,0 +1,22 @@
+/*
+*
+*   Things to add to r!meme:
+* ----------------------------
+*
+*/
+
+const Discord = require("discord.js");
+
+exports.run = async (message) => {
+    if(message.content.toLowerCase() == "rb!user") {
+
+        if(member.user.premium == true) {
+            return message.channel.send("Has Nitro");
+        } else {
+            return message.channel.send("No Nitro");
+        }
+        let user_embed = new Discord.RichEmbed()
+        .setColor("#EB671D")
+        .setTitle(message.user);
+    }
+}
