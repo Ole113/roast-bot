e57ea91e13777e61897d6aa5a32d1e3bfe65c352
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-24 20:06:46 -0600

    update

--- Commands/user.js
+++ Commands/user.js
@@ -27,13 +27,20 @@ exports.run = async (message) => {
                 status = "<:dnd:493892741613355008>  Do Not Disturb";
                 break;
         }
+
+        var game = 0;
+        if(message.author.presence.game == null) {
+            game = "None";
+        } else {
+            game = message.author.presence.game;
+        }
         let user_embed = new Discord.RichEmbed()
         .setColor("#EB671D")
         .setTitle(`${message.author.username}'s Stats:`)
         .setThumbnail(message.author.displayAvatarURL)
         .addField("Account created at: ", message.author.createdAt.toString())
         .addField("User Id:", message.author.id)
-        .addField("Current Game:", message.author.presence.game)
+        .addField("Current Game:", game)
         .addField("Current Presense:", status);
 
         //return message.channel.send(message.author.createdAt.toString());
