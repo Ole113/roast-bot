b9c34ec6b875fd4056cad32a7bc1302e0bca3e6b
Ole113 <alex_elbel@icloud.com>
2018-09-26 15:37:59 -0600

    Update user.js

--- Commands/user.js
+++ Commands/user.js
@@ -47,7 +47,7 @@ if(message.author.presence.game == null) {
         .addField("User Id:", user.id)
         //.addField("Current Game:", game)
         .addField("Bot:", user.bot.toString())
-        .addField("Current Presense:", message.mentions.users.presence.status);
+        .addField("Current Presense:", message.mentions.presence.status);
         return message.channel.send(user_embed);
 
     });
