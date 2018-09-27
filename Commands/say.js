c7899707508e85fef49e8764f674e2724a0d34a0
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-26 18:50:20 -0600

    v2.3.0 is live! r!user

--- Commands/say.js
+++ Commands/say.js
@@ -12,9 +12,10 @@ exports.run = async (message) => {
     if(message.content.toLowerCase() == "rb!say" || message.content.toLowerCase() == "r!say  "){
         return message.channel.send("Please provide what you want Roast-Bot to say. The correct usage is `r!say whatToSay`. ");
     }
-    if(message.content.toLowerCase() == "rb!say help") {
-        return message.channel.send("coming soon");
-    }
+   if(message.content.toLowerCase() == "r!say help") {
+    return message.channel.send("**r!say help:**\n\nTo use `r!say whatToSay` “whatToSay” is want you want the bot to say.\n\n*Example:*\n\nUSER: r!say Hello!\nRoast-Bot: Hello!\n\nIf you accidentally use `r!say` with one or more spaces after r!say without saying what you want Roast-Bot to say, Roast-Bot will return a warning of “Please provide what you want Roast-Bot to say. The correct usage is `r!say whatToSay`.”\n\nStill having trouble with r!meme or have a suggestion? Join the support server:\nhttps://discordapp.com/invite/9y8yV42");
+}
+
     if (message.content.toLowerCase().startsWith("rb!say ")) {
         const word = message.content;
         const say = word.slice(6, word.length);
--- Commands/user.js
+++ Commands/user.js
@@ -12,79 +12,79 @@ const client = new Discord.Client();
 exports.run = async (message) => {
 
 
-if(message.content.toLowerCase().startsWith("rb!user ")) {
-    var muser = message.mentions.users;
-        
-    muser.forEach(function(users){
-         var statuss = "default";
-         switch(users.presence.status) {
-                case "online":
-                        statuss = "<:online:493891715678339089>  Online";
-                        break;
-                case "offline":
-                        statuss = "<:invisible:493897783179214858>  Offline";
-                        break;
-                case "idle":
-                        statuss = "<:idle:493892777944285194>  Idle";
-                        break;
-                case "dnd":
-                        statuss = "<:dnd:493892741613355008>  Do Not Disturb";
-                        break;
-        }
-        
-        var gamee = 0;
-        if(users.presence.game == null) {
-        gamee = "None";
-        } else {
-            gamee = users.presence.game;
-        }
-        
-        let user_embed = new Discord.RichEmbed()
-        .setColor("#EB671D")
-        .setTitle(`${users.username}'s Stats:`)
-        .setThumbnail(users.displayAvatarURL)
-        .addField("Account created at: ", users.createdAt.toString())
-        .addField("User Id:", users.id)
-        .addField("Current Game:", gamee)
-        .addField("Bot:", users.bot.toString())
-        .addField("Current Presense:", statuss);
-        return message.channel.send(user_embed);
-        
-        
-    });
-} 
+    if (message.content.toLowerCase().startsWith("rb!user ")) {
+        var muser = message.mentions.users;
 
-if(message.content.toLowerCase() == "rb!user") {
+        muser.forEach(function (users) {
+            var statuss = "default";
+            switch (users.presence.status) {
+                case "online":
+                    statuss = "<:online:493891715678339089>  Online";
+                    break;
+                case "offline":
+                    statuss = "<:invisible:493897783179214858>  Offline";
+                    break;
+                case "idle":
+                    statuss = "<:idle:493892777944285194>  Idle";
+                    break;
+                case "dnd":
+                    statuss = "<:dnd:493892741613355008>  Do Not Disturb";
+                    break;
+            }
+
+            var gamee = 0;
+            if (users.presence.game == null) {
+                gamee = "None";
+            } else {
+                gamee = users.presence.game;
+            }
+
+            let user_embed = new Discord.RichEmbed()
+                .setColor("#EB671D")
+                .setTitle(`${users.username}'s Stats:`)
+                .setThumbnail(users.displayAvatarURL)
+                .addField("Account created at: ", users.createdAt.toString())
+                .addField("User Id:", users.id)
+                .addField("Current Game:", gamee)
+                .addField("Bot:", users.bot.toString())
+                .addField("Current Presense:", statuss);
+            return message.channel.send(user_embed);
+
+
+        });
+    }
+
+    if (message.content.toLowerCase() == "rb!user") {
         var status = "default";
-switch(message.author.presence.status) {
-    case "online":
-        status = "<:online:493891715678339089>  Online";
-        break;
-    case "offline":
-        status = "<:invisible:493897783179214858>  Offline";
-        break;
-    case "idle":
-        status = "<:idle:493892777944285194>  Idle";
-        break;
-    case "dnd":
-        status = "<:dnd:493892741613355008>  Do Not Disturb";
-        break;
-}
-var game = 0;
-if(message.author.presence.game == null) {
-    game = "None";
-} else {
-    game = message.author.presence.game;
-}
+        switch (message.author.presence.status) {
+            case "online":
+                status = "<:online:493891715678339089>  Online";
+                break;
+            case "offline":
+                status = "<:invisible:493897783179214858>  Offline";
+                break;
+            case "idle":
+                status = "<:idle:493892777944285194>  Idle";
+                break;
+            case "dnd":
+                status = "<:dnd:493892741613355008>  Do Not Disturb";
+                break;
+        }
+        var game = 0;
+        if (message.author.presence.game == null) {
+            game = "None";
+        } else {
+            game = message.author.presence.game;
+        }
         let user_embed = new Discord.RichEmbed()
-        .setColor("#EB671D")
-        .setTitle(`${message.author.username}'s Stats:`)
-        .setThumbnail(message.author.displayAvatarURL)
-        .addField("Account created at: ", message.author.createdAt.toString())
-        .addField("User Id:", message.author.id)
-        .addField("Current Game:", game)
-        .addField("Bot:", message.author.bot)
-        .addField("Current Presense:", status);
+            .setColor("#EB671D")
+            .setTitle(`${message.author.username}'s Stats:`)
+            .setThumbnail(message.author.displayAvatarURL)
+            .addField("Account created at: ", message.author.createdAt.toString())
+            .addField("User Id:", message.author.id)
+            .addField("Current Game:", game)
+            .addField("Bot:", message.author.bot)
+            .addField("Current Presense:", status);
         return message.channel.send(user_embed);
 
     }
