e0bfe7d2be8cfb59be60018800d4f328516fd0b2
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-28 16:27:56 -0600

    update

--- Database/custom-prefix.js
+++ Database/custom-prefix.js
@@ -13,7 +13,7 @@ exports.run = async (message) => {
 			const key = `${message.guild.id}-${message.author.id}`;
 			if(!customPrefix.has(key)) {
 				customPrefix.set(key, {
-					user: message.author.id, guild: message.guild.id, prefix: custom_prefix
+					user: message.author.id, guild: message.guild.id, prefix: "r!"
 				});
 			}
 			let currentPrefix = customPrefix.get(key, "prefix");
@@ -22,7 +22,7 @@ exports.run = async (message) => {
 
 			return message.channel.send(customPrefix);
 			
-			if(message.content.toLowerCase() === "r!prefix") {
+			if(message.content.toLowerCase() === "rb!prefix") {
 				const key = `${message.guild.id}-${message.author.id}`;
 				let currentPrefix = customPrefix.get(key, "prefix");
 				return message.channel.send(`Custom Prefix: ${customPrefix.get(key, "prefix")} .`);
