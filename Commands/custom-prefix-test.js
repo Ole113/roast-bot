6f954544e87ce5b715deaf2d273659adc39713ad
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-28 17:40:57 -0600

    tset

--- Commands/custom-prefix-test.js
+++ Commands/custom-prefix-test.js
@@ -1,7 +1,7 @@
-const customP = require("./Database/custom-prefix.json");
+const customP = require("./custom-prefix.json");
 
 exports.run = async (message) => {
     if(message.content.toLowerCase() == customP.prefix + "test"){
-        return message.channel.send(`Custom Prefix is ${customP.prefix}`);
+        return message.channel.send(`Prefix is ${customP.prefix}`);
     }
 }
