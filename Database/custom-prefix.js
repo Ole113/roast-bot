4308c60259cced573e4862094d3a889751e92084
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-28 17:14:45 -0600

    update'

--- Database/custom-prefix.js
+++ Database/custom-prefix.js
@@ -21,7 +21,7 @@ exports.run = async (message) => {
 			
 			customPrefix.set(key, custom_prefix, "prefix")
 
-			return message.channel.send(`Custom Prefix set to *${customPrefix.get(key, "prefix")}`);
+			return message.channel.send(`Custom Prefix set to *${customPrefix.get(key, "prefix")}*`);
 			
 		}
 	});
