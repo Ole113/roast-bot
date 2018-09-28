07097053f699ec74fb8a9353b0b5025785f9570b
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-28 14:20:20 -0600

    roast-bot website coming soon

---
+++ Website/main.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    const commands = document.getElementById("commands");
+    const commandHelp = document.getElementById("commandHelp");
+    const info = document.getElementById("info");
+ 
+     info.addEventListener("click", function() {
+         alert(2)
+     });
+     
+     commandHelp.addEventListener("click", function() {
+         alert(2)
+     })
+     
+     commands.addEventListener("click", function() {
+         alert(2)
+     });
+ }
