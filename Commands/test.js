2a28aaff3dc9618ddd419e62250c8ae842e4c814
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-09-29 10:38:08 -0600

    update

--- Commands/test.js
+++ Commands/test.js
@@ -1,7 +1,8 @@
-const prefix_file = require("../Database/prefix.js");
+const prefix_file = require("../Database/prefix.json");
 
 exports.run = async (message) => {
     if(message.content.toLowerCase == prefix_file.prefix + "test") {
+        console.log(prefix_file.prefix);
         return message.channel.send("Worked");
     }
 }
--- Database/prefix.js
+++
@@ -1,1 +0,0 @@
-var prefix = "r!";
