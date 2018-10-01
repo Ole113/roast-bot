05acf91250ca7bf085eb998597ca0523336f217b
Ole113 <alex_elbel@icloud.com>
2018-10-01 07:49:29 -0600

    Create on-off.js

---
+++ Database/on-off.js
@@ -0,0 +1,9 @@
+const Enmap = require("enmap");
+const customPrefix = new Enmap({name: "custom-prefix"});
+const bsp = require("better-sqlite-pool");
+
+const prefix_file = require("./prefix.json");
+
+exports.run = async (message) => {
+
+}
