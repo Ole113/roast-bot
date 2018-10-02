5222570bdb484b37ca3cd31a86b8dff3d6aa203e
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-01 18:13:30 -0600

    Initial Commit

--- Website/main.js
+++ Website/main.js
@@ -1,17 +1,3 @@
 window.onload = function() {
-    const commands = document.getElementById("commands");
-    const commandHelp = document.getElementById("commandHelp");
-    const info = document.getElementById("info");
- 
-     info.addEventListener("click", function() {
-         //alert(2)
-     });
-     
-     commandHelp.addEventListener("click", function() {
-         //alert(2)
-     })
-     
-     commands.addEventListener("click", function() {
-         //alert(2)
-     });
- }
+
+}
