6efbea5d93541cbfee26562524f6d1e3a425fa21
Ole113 <alex_elbel@icloud.com>
2018-10-05 08:21:00 -0600

    Update custom-roast.js

--- Database/custom-roast.js
+++ Database/custom-roast.js
@@ -9,18 +9,21 @@ exports.run = async (message) => {
         if(message.author.bot) return;
         
         if(message.content.toLowerCase().startsWith(prefix_file.prefix + "cr ")) {
-           
+		
+            const key = `${message.guild.id}-${message.author.id}`;
+		
+	    if(!customRoast.has(key)) {
+		customRoast.set(key, {
+			user: message.author.id, guild: message.guild.id, number: 2, roast: 1
+		});
+            }
+		
             let content = message.content;
             let custom_roast = content.slice(prefix_file.prefix.length + 3, content.length);
 
 	    let number = customRoast.get(key, "number");	
 	
-            const key = `${message.guild.id}-${message.author.id}`;
-			if(!customRoast.has(key)) {
-			  customRoast.set(key, {
-				user: message.author.id, guild: message.guild.id, number: 2, roast: 1
-			  });
-            }
+
             
             customRoast.set(key, custom_roast, "roast");
 	    customRoast.set(key, --number, "number");
