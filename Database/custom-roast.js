cad9fcce85ffd18b489031f296a145e7d455cc8b
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-08 16:55:56 -0600

    update

--- Database/custom-roast.js
+++ Database/custom-roast.js
@@ -1,42 +1,42 @@
 const Enmap = require("enmap");
-const customRoast = new Enmap({name: "custom-roast"});
+const customRoast = new Enmap({ name: "custom-roast" });
 const bsp = require("better-sqlite-pool");
 
 const prefix_file = require("./prefix.json");
 
 exports.run = async (message) => {
 	customRoast.defer.then(() => {
-        if(message.author.bot) return;
-        
-        if(message.content.toLowerCase().startsWith(prefix_file.prefix + "cr ")) {
-		
-            const key = `${message.guild.id}-${message.author.id}`;
-		
-	    if(!customRoast.has(key)) {
-		customRoast.set(key, {
-			user: message.author.id, guild: message.guild.id, number: 0, roast: 1
-		});
-            }
-		
-	    if(message.content.toLowerCase() == prefix_file.prefix + "cr") {
-		let random_number = Math.random() * customRoast.get(key, "number");
-		return message.channel.send(`${customRoast.get(key, "roast")} \n Meme #${customRoast.get(key, "number")}`);
-	    }
-		
-            let content = message.content;
-            let custom_roast = content.slice(prefix_file.prefix.length + 3, content.length);
+		if (message.author.bot) return;
 
-	    let number = customRoast.get(key, "number");	
-	
+		if (message.content.toLowerCase().startsWith(prefix_file.prefix + "cr ")) {
 
-            
-            customRoast.set(key, custom_roast, "roast");
-	    customRoast.set(key, ++number, "number");
-	    if(customRoast.get(key, "number") < 3) {
-            	return message.channel.send(`Custom Roast #${customRoast.get(key, "number")} and has been set to *${customRoast.get(key, "roast")}*`);
-	    } else {
-		return message.channel.send("You have run out of free custom roasts. To get unlimited upgrade for only $1.50.");
-	    }
-        }
-    });
+			const key = `${message.guild.id}-${message.author.id}`;
+
+			if (!customRoast.has(key)) {
+				customRoast.set(key, {
+					user: message.author.id, guild: message.guild.id, number: 0, roast: 1
+				});
+			}
+
+
+
+			let content = message.content;
+			let custom_roast = content.slice(prefix_file.prefix.length + 3, content.length);
+
+			let number = customRoast.get(key, "number");
+
+			if (message.content.toLowerCase() == prefix_file.prefix + "cr") {
+				let random_number = Math.random() * customRoast.get(key, "number");
+				return message.channel.send(`${customRoast.get(key, "roast")} \n Meme #${customRoast.get(key, "number")}`);
+			}
+
+			customRoast.set(key, custom_roast, "roast");
+			customRoast.set(key, ++number, "number");
+			if (customRoast.get(key, "number") < 3) {
+				return message.channel.send(`Custom Roast #${customRoast.get(key, "number")} and has been set to *${customRoast.get(key, "roast")}*`);
+			} else {
+				return message.channel.send("You have run out of free custom roasts. To get unlimited upgrade for only $1.50.");
+			}
+		}
+	});
 }
