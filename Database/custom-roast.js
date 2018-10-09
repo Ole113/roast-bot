ce767596c3e2b5007d7288e025d961d04796da49
Ole113 <alex.e00113@gmail.com>
2018-10-09 08:10:52 -0600

    Update custom-roast.js

--- Database/custom-roast.js
+++ Database/custom-roast.js
@@ -27,7 +27,7 @@ exports.run = async (message) => {
 			let content = message.content;
 		    	let content_slice = content.slice(prefix_file.prefix.length + 15, content.length);
 			let number_int = parseInt(content_slice);
-			custom_roasts[number_int] = "You haven't set this custom roasts yet! Use `r!cr help` to learn how to.";
+			custom_roasts[number_int - 1] = "You haven't set this custom roasts yet! Use `r!cr help` to learn how to.";
 			return message.channel.send(`Custom roast #${number_int} was removed successfully!`);
 
 		}
