7312426acf4c32c600ee0633918bcc9909a35e17
Ole113 <alex.e00113@gmail.com>
2018-10-09 08:39:49 -0600

    Update on-off.js

--- Database/on-off.js
+++ Database/on-off.js
@@ -1,9 +1,24 @@
 const Enmap = require("enmap");
-const customPrefix = new Enmap({name: "custom-prefix"});
+const onOff = new Enmap({name: "on-off"});
 const bsp = require("better-sqlite-pool");
 
 const prefix_file = require("./prefix.json");
+const onOff_file  = require("./on-off.json");
 
 exports.run = async (message) => {
+	onOff.defer.then(() => {
+		if (message.author.bot) return;
+		
+		const key = `${message.guild.id}-${message.author.id}`;
 
+		if (!onOff.has(key)) {
+			onOff.set(key, {
+				user: message.author.id, guild: message.guild.id
+			});
+			
+		if(message.content.toLowerCase().startsWith(prefix_file.prefix + "on")) {
+				
+		}
+		}
+	}	 
 }
