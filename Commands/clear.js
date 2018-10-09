4124baf89d4af87183315ab8b7cfdb5bcf418629
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-08 18:01:39 -0600

    max clear fix

--- Commands/clear.js
+++ Commands/clear.js
@@ -20,7 +20,7 @@ exports.run = async (message) => {
         }
         if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
             return message.channel.send("Roast-Bot needs to be given Manage Messages permissions to use this command :( <:roast_circle:474755210485563404>");
-        } else if (int >= 100) {
+        } else if (int > 100) {
             return message.channel.send("The max number of messages you can delete is 100 :( <:roast_circle:474755210485563404>");
         } else if (!message.member.hasPermission("MANAGE_MESSAGES")) {
             return message.channel.send("Looks like you dont have the permissions to do that :( <:roast_circle:474755210485563404>");
