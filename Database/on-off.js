6661ae81d5a25336d3b40a564fa9930ba71d6418
Ole113 <alex_elbel@icloud.com>
2018-10-10 15:26:53 -0600

    Update on-off.js

--- Database/on-off.js
+++ Database/on-off.js
@@ -13,12 +13,23 @@ exports.run = async (message) => {
 
 		if (!onOff.has(key)) {
 			onOff.set(key, {
-				user: message.author.id, guild: message.guild.id
+				user: message.author.id, guild: message.guild.id, "roast": "on", "meme": "on", "say": "on", "user": "on", "urban": "on", "clear": "on", "server": "on", "bot": "on"
 			});
 			
 		if(message.content.toLowerCase().startsWith(prefix_file.prefix + "on")) {
-				
+			const roast = onOff.get(key, "roast");
+			const meme = onOff.get(key, "meme");
+			const say = onOff.get(key, "say");
+			const user = onOff.get(key, "user");
+			const urban = onOff.get(key, "urban");
+			const clear = onOff.get(key, "clear");
+			const server = onOff.get(key, "server");
+			const bot = onOff.get(key, "bot");
+
+			let content = message.content;
+			let command = content.slice(prefix_file.prefix.length + 3, content.length);
+			
+			return message.channel.send(`Prefix was ${command}`);
 		}
-		}
-	}	 
+	} 
 }
