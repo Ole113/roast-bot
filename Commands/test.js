ae6bf1a9d098d6fee0dd33abb656f027f81b1f80
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-10 20:30:24 -0600

    mistype fix

--- Commands/test.js
+++ Commands/test.js
@@ -2,7 +2,7 @@ const test = require("../Database/on-off.json");
 const prefix_file = require("../Database/on-off.js");
 
 exports.run = async (message) => {
-    if(message.author.toLowerCase().startsWith(prefix_file.prefix + "test") && test.meme == "on") {
+    if(message.content.toLowerCase().startsWith(prefix_file.prefix + "test") && test.meme == "on") {
         return message.channel.send(`${test.meme} is turned on yay`);
     }
 }
