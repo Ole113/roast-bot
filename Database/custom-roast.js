7b78a91fe80297054ba23af77d05b038df1eeb67
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-13 10:41:23 -0600

    update

--- Database/custom-roast.js
+++ Database/custom-roast.js
@@ -13,9 +13,13 @@ const custom_roasts = [
 exports.run = async (message) => {
 	customRoast.defer.then(() => {
 		if (message.author.bot) return;
+		const random_croast = 1;
 		if (message.content.toLowerCase() == prefix_file.prefix + "croast") {
-			const random_croast = Math.ceil(Math.random() * 3);
-			return message.channel.send(custom_roasts[random_croast - 1].roast + `\n **Roast #${random_croast}** <:roast_circle:474755210485563404>`);
+			random_croast++;
+			if(random_croast > 3) {
+				random_croast = 1;
+			}
+			return message.channel.send(custom_roasts[random_croast - 1].roast + `\n **Custom Roast #${random_croast}** <:roast_circle:474755210485563404>`);
 		}
 		if (message.content.toLowerCase().startsWith(prefix_file.prefix + "croast delete #")) {
 			let content = message.content;
