ef1b0520bacc89172f4cdfd878b79abb60f51115
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-17 21:41:14 -0600

    # for vid

--- Commands/vid.js
+++ Commands/vid.js
@@ -1,21 +1,26 @@
- /*
+/*
 *
 *   Things to add to rb!invite:
 * ----------------------------
 * 
 */
- 
+
 const Discord = require("discord.js");
- 
+
 const prefixFile = require("../Database/prefix.json");
- 
+
 exports.run = async (message) => {
     if (message.author.bot) return;
-    if(message.content.toLowerCase() == prefixFile.prefix + "vid help"){
+    if (message.content.toLowerCase() == prefixFile.prefix + "vid help") {
         return message.channel.send("coming soon");
     }
-if(message.content.toLowerCase() == prefixFile.prefix + "vid") {
-    let randomNumber = Math.floor(Math.random() * 7);
-    return message.channel.send(`Video #${randomNumber + 1} <:roast_circle:474755210485563404>`, { files: [`Videos/vid${randomNumber + 1}.gif`] });
+    if (message.content.toLowerCase() == prefixFile.prefix + "vid") {
+        let randomNumber = Math.floor(Math.random() * 7);
+        return message.channel.send(`Video #${randomNumber + 1} <:roast_circle:474755210485563404>`, { files: [`Videos/vid${randomNumber + 1}.gif`] });
+    }
+    if (message.content.startsWith(prefixFile.prefix + "vid #")) {
+        let content = message.content;
+        let vidNumber = content.slice(prefixFile.prefix + 5, content.length);
+        return message.channel.send(`Video #${vidNumber + 1} <:roast_circle:474755210485563404>`, { files: [`Videos/vid${vidNumber + 1}.gif`] });
     }
 }
