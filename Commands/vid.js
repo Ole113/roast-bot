2b031debff7596bbe3e6f204b08269bae54769ad
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-17 21:44:24 -0600

    update

--- Commands/vid.js
+++ Commands/vid.js
@@ -21,6 +21,7 @@ exports.run = async (message) => {
     if (message.content.startsWith(prefixFile.prefix + "vid #")) {
         let content = message.content;
         let vidNumber = content.slice(prefixFile.prefix + 5, content.length);
-        return message.channel.send(`Video #${vidNumber + 1} <:roast_circle:474755210485563404>`, { files: [`Videos/vid${vidNumber + 1}.gif`] });
+        let vidNumberInt = Number(vidNumber);
+        return message.channel.send(`Video #${vidNumberInt + 1} <:roast_circle:474755210485563404>`, { files: [`Videos/vid${vidNumberInt + 1}.gif`] });
     }
 }
