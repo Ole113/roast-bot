dc95a28e61a5163e8525f1c2a83488227d383e8b
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-17 22:40:03 -0600

    update

--- Commands/vid.js
+++ Commands/vid.js
@@ -16,12 +16,15 @@ exports.run = async (message) => {
     }
     if (message.content.toLowerCase() == prefixFile.prefix + "vid") {
         let randomNumber = Math.floor(Math.random() * 7);
-        return message.channel.send(`Video #${randomNumber} <:roast_circle:474755210485563404>`, { files: [`Videos/vid${randomNumber + 1}.gif`] });
+        return message.channel.send(`Video #${randomNumber} <:roast_circle:474755210485563404>`, { files: [`Videos/vid${randomNumber}.gif`] });
     }
     if (message.content.startsWith(prefixFile.prefix + "vid #")) {
         let content = message.content;
         let vidNumber = content.slice(prefixFile.prefix.length + 5, content.length);
         let vidNumberInt = parseInt(vidNumber);
+        if(vidNumberInt > 7 || vidNumberInt <= 0) {
+            return message.channel.send("Sorry that video couldn't be found. <:roast_circle:474755210485563404>");
+        }
         return message.channel.send(`Video #${vidNumberInt} <:roast_circle:474755210485563404>`, { files: [`Videos/vid${vidNumberInt}.gif`] });
     }
 }
