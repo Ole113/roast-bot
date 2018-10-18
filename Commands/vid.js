5f37bcc186a5a0cf75d719302ddcf21efdbb5429
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-17 20:37:49 -0600

    mistype fix

--- Commands/vid.js
+++ Commands/vid.js
@@ -15,6 +15,6 @@ exports.run = async (message) => {
         return message.channel.send("coming soon");
     }
 if(message.content.toLowerCase() == prefixFile.prefix + "vid") {
-    return message.channel.send(`Video #${1} <:roast_circle:474755210485563404>`, { files: [`Videos/vid1.gifs`] });
+    return message.channel.send(`Video #${1} <:roast_circle:474755210485563404>`, { files: [`Videos/vid1.gif`] });
     }
 }
