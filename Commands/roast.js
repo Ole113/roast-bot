3193dafd7b3a759d2e677834fd56161bf1ac6914
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-20 13:18:21 -0600

    custom roasts live soon

--- Commands/roast.js
+++ Commands/roast.js
@@ -122,8 +122,7 @@ exports.run = async (message) => {
 	}
 	if (message.content.toLowerCase().startsWith(prefixFile.prefix + "roast") && onOffFile.roast == "on") {
 		if (message.content.toLowerCase() == prefixFile.prefix + "roast") {
-			//const randomRoasts = Math.ceil(Math.random() * 100 + customRoastFile.length);
-			const randomRoasts = 101;
+			const randomRoasts = Math.ceil(Math.random() * 100 + customRoastFile.length);
 			if (randomRoasts >= 101) {
 				return message.channel.send(customRoastFile[randomRoasts - 101].roast + `\n **Custom Roast #${randomRoasts - 100}** <:roast_circle:474755210485563404>`);
 			} else {
@@ -131,7 +130,7 @@ exports.run = async (message) => {
 			}
 		} else if (message.content.toLowerCase().startsWith(prefixFile.prefix + "roast ")) {
 			//const random = Math.ceil(Math.random() * 100 + customRoastFile.length);
-			const random = 101;
+			const random = 102;
 			const word = message.content;
 			const reply = word.slice(prefixFile.prefix.length + 6, word.length);
 			if (message.content.toLowerCase().startsWith(prefixFile.prefix + "roast #")) {
