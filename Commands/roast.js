7f7391c7a4cd21c32702680076fcd9db2ea7e9e8
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-19 23:01:24 -0600

    tester for custom roasts

--- Commands/roast.js
+++ Commands/roast.js
@@ -122,14 +122,18 @@ exports.run = async (message) => {
 	}
 	if (message.content.toLowerCase().startsWith(prefixFile.prefix + "roast") && onOffFile.roast == "on") {
 		if (message.content.toLowerCase() == prefixFile.prefix + "roast") {
-			const randomRoasts = Math.ceil(Math.random() * 100 + customRoastFile.length);
+			//const randomRoasts = Math.ceil(Math.random() * 100 + customRoastFile.length);
+			const randomRoasts = 101;
+			randomRoasts++;
 			if (randomRoasts > 101) {
 				return message.channel.send(customRoastFile[randomRoasts - 101].roast + `\n **Custom Roast #${randomRoasts - 100}** <:roast_circle:474755210485563404>`);
 			} else {
 				return message.channel.send(roasts[randomRoasts].roast + `\n **Roast #${randomRoasts}** <:roast_circle:474755210485563404>`);
 			}
 		} else if (message.content.toLowerCase().startsWith(prefixFile.prefix + "roast ")) {
-			const random = Math.ceil(Math.random() * 100 + customRoastFile.length);
+			//const random = Math.ceil(Math.random() * 100 + customRoastFile.length);
+			const random = 101;
+			random++;
 			const word = message.content;
 			const reply = word.slice(prefixFile.prefix.length + 6, word.length);
 			if (message.content.toLowerCase().startsWith(prefixFile.prefix + "roast #")) {
