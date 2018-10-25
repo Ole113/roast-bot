e8069486d0cbf472f052454699ed829bc2e7224a
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-24 20:31:57 -0600

    help message embed update

--- Commands/help.js
+++ Commands/help.js
@@ -59,7 +59,7 @@ exports.run = async (client, message) => {
                     if (reaction.emoji.name === "▶") {
                     let secondPage = new Discord.RichEmbed()
                         secondPage.addField("Test field", "tset field");
-                        reactions.edit({ embed: secondPage });
+                        reactions.edit(secondPage);
                     }
                 });
         }).catch(e => {
