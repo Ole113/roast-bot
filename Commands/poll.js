9a11e90f6d279b0994608fc59759b4b40958a5ff
Ole113 <alex_elbel@icloud.com>
2018-10-25 08:19:33 -0600

    Update poll.js

--- Commands/poll.js
+++ Commands/poll.js
@@ -1,1 +1,19 @@
-//update
+/*
+*
+*   Things to add to r!poll:
+* ----------------------------
+*
+*/
+
+const Discord = require("discord.js");
+
+const prefixFile = require("../Database/prefix.json");
+
+exports.run = async (message) => {
+    if(message.content.toLowerCase().startsWith(prefixFile.prefix + "poll")) {
+        let pollEmbed = new Discord.RichEmbed()
+            .setColor("#EB671D")
+            .setTitle(`Poll Started by ${message.author}`);
+        return message.channel.send({ embed: pollEmbed });
+    }
+}
