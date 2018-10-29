e55e7f724782dca69f80f3e64ea5c070b9290756
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-28 19:12:59 -0600

    ADMIN antny got roast bot alpha back up and working. exe file udated

--- main.js
+++ main.js
@@ -7,29 +7,30 @@
 *  Add a rb!user @USER and have stats such as if they have nitro, servers in, date joined disord and other info
 *  Make it so when it has try catch block for commands if will eventaully send errors to db. 
 *
+*  ALEX DO GUD IN PROGRAMING NERD STUFF U R BEST,for what u r
 */
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const botFile = require("./Commands/bot.js");
-const clearFile = require("./commands/clear.js");
-const serverFile = require("./commands/server.js");
-const inviteFile = require("./commands/invite.js");
-const helpFile = require("./commands/help.js");
-const roastFile = require("./commands/roast.js");
-const memeFile = require("./commands/meme.js");
-const sayFile = require("./commands/say.js");
-const urbanFile = require("./commands/urban");
-const xpLevelFile = require("./database/xp-level.js");
-const userFile = require("./commands/user.js");
-const customPrefixFile = require("./database/custom-prefix.js");
-const prefixFile = require("./database/prefix.json");
-const feedbackFile = require("./database/feedback.js");
-const customRoastFile = require("./database/custom-roast.js");
-const onOffFile = require("./database/on-off.js");
-const vidFile = require("./commands/vid.js");
-const pollFile = require("./commands/poll.js");
-const websiteFile = require("./commands/website.js");
+const clearFile = require("./Commands/clear.js");
+const serverFile = require("./Commands/server.js");
+const inviteFile = require("./Commands/invite.js");
+const helpFile = require("./Commands/help.js");
+const roastFile = require("./Commands/roast.js");
+const memeFile = require("./Commands/meme.js");
+const sayFile = require("./Commands/say.js");
+const urbanFile = require("./Commands/urban");
+const xpLevelFile = require("./Database/xp-level.js");
+const userFile = require("./Commands/user.js");
+const customPrefixFile = require("./Database/custom-prefix.js");
+const prefixFile = require("./Database/prefix.json");
+const feedbackFile = require("./Database/feedback.js");
+const customRoastFile = require("./Database/custom-roast.js");
+const onOffFile = require("./Database/on-off.js");
+const vidFile = require("./Commands/vid.js");
+const pollFile = require("./Commands/poll.js");
+const websiteFile = require("./Commands/website.js");
 
 const botPrecense = [
 	"http://roast-bot.com",
