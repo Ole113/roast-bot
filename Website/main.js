9c2e4eb694776bd6adcef31bc0d5957d6a181ce2
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-28 20:00:20 -0600

    express added

--- Website/main.js
+++ Website/main.js
@@ -1,3 +1,14 @@
-window.onload = function() {
+var express = require("express");
+var app = express();
+var path = require("path");
 
-};
+app.set('port', (process.env.PORT || 80))
+app.use(express.static("./website"))
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "./website/index.html"));
+});
+
+app.listen(app.get("port"), function() {
+  console.log("Node app is running at localhost:" + app.get("port"))
+});
