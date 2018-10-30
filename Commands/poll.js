e8b9f58a0538fc8220f22b41fd2149f5dee23d5a
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-29 21:26:45 -0600

    poll command test update

--- Commands/poll.js
+++ Commands/poll.js
@@ -11,32 +11,33 @@ const prefixFile = require("../Database/prefix.json");
 
 exports.run = async (message) => {
     if (message.content.toLowerCase().startsWith(prefixFile.prefix + "poll")) {
-        const reactionFilter = (reaction, user) => reaction.emoji.name === '✅';
+        const reactionFilter = (reaction, user) => reaction.emoji.name === "✅";
 
         const embed = new Discord.RichEmbed({
-            title: 'Suggestion by someone',
-            description: 'This is a test suggestion. Can you please like it or dislike it :)',
+            title: "Suggestion by someone",
+            description: "This is a test suggestion. Can you please like it or dislike it :)",
             fields: [
-                {name: 'Like:', value: '<3'}
+                {name: "Like:", value: "<3"},
+                {name: "a test", value: "test"}
             ]
         });
         
         // add reaction emoji to message
         message.channel.send(embed)
-        .then(msg => msg.react('✅'))
-        .then(mReaction => mReaction.message.react('❎') )
+        .then(msg => msg.react("✅"))
+        .then(mReaction => mReaction.message.react("❎") )
         .then(mReaction => {
             // createReactionCollector - responds on each react, AND again at the end.
             const collector = mReaction.message
                 .createReactionCollector(reactionFilter, { time: 15000 });
         
             // set collector events
-            collector.on('collect', r => {
-                // immutably copy embed's Like field to new obj
+            collector.on("collect", r => {
+                // immutably copy embed"s Like field to new obj
                 let embedLikeField = Object.assign({}, embed.fields[0]);
         
-                // update 'field' with new value
-                embedLikeField.value = '<3 <3 <3';
+                // update "field" with new value
+                embedLikeField.value = "<3 <3 <3";
         
                 // create new embed with old title & description, new field
                 const newEmbed = new Discord.RichEmbed({
