e58c323394aff4253cb3cc949533024cd312cdf6
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-29 19:52:36 -0600

    poll test update

--- Commands/poll.js
+++ Commands/poll.js
@@ -10,35 +10,47 @@ const Discord = require("discord.js");
 const prefixFile = require("../Database/prefix.json");
 
 exports.run = async (message) => {
-    if(message.content.toLowerCase().startsWith(prefixFile.prefix + "poll")) {
-        return message.channel.send("Poll started.")
-        .then(async (reaction) => {
-            reaction.react('👍').then(() => reaction.react('👎'));
+    if (message.content.toLowerCase().startsWith(prefixFile.prefix + "poll")) {
+        const reactionFilter = (reaction, user) => reaction.emoji.name === '✅';
 
-            const filter = (reactions) => {
-                return ['👍', '👎'].includes(reactions.emoji.name);
-            };
-
-            reaction.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-                .then(collected => {
-                    const reaction = collected.first();
-
-                    if (reaction.emoji.name === '👍') {
-                        reaction.reply('you reacted with a thumbs up.');
-                    }
-                    else {
-                        reaction.reply('you reacted with a thumbs down.');
-                    }
-                })
-                .catch(collected => {
-                    console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
-                    reaction.reply('you didn\'t react with neither a thumbs up, nor a thumbs down.');
-                });
+        const embed = new Discord.RichEmbed({
+            title: 'Suggestion by someone',
+            description: 'This is a test suggestion. Can you please like it or dislike it :)',
+            fields: [
+                { name: 'Like:', value: '<3' }
+            ]
         });
 
-            //await reactions.react("◀");
-            //await reactions.react("▶");
+        // add reaction emoji to message
+        message.channel.send(embed)
+            .then(msg => msg.react('✅'))
+            .then(mReaction => mReaction.message.react('❎'))
+            .then(mReaction => {
+                // createReactionCollector - responds on each react, AND again at the end.
+                const collector = mReaction.message
+                    .createReactionCollector(reactionFilter, { time: 15000 });
 
+                // set collector events
+                collector.on('collect', r => {
+                    // immutably copy embed's Like field to new obj
+                    let embedLikeField = Object.assign({}, embed.fields[0]);
 
+                    // update 'field' with new value
+                    embedLikeField.value = '<3 <3 <3';
+
+                    // create new embed with old title & description, new field
+                    const newEmbed = new Discord.RichEmbed({
+                        title: embed.title,
+                        description: embed.description,
+                        fields: [embedLikeField]
+                    });
+
+                    // edit message with new embed
+                    // NOTE: can only edit messages you author
+                    r.message.edit(newEmbed);
+                    //await reactions.react("◀");
+                    //await reactions.react("▶");
+                });
+            });
     }
 }
