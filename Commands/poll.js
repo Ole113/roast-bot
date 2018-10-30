42c6a3b86a03c922d625d6f6a1c08e1091efe8dd
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-10-29 20:01:13 -0600

    should work now

--- Commands/poll.js
+++ Commands/poll.js
@@ -11,42 +11,35 @@ const prefixFile = require("../Database/prefix.json");
 
 exports.run = async (message) => {
     if (message.content.toLowerCase().startsWith(prefixFile.prefix + "poll")) {
-        const reactionFilter = (reaction, user) => reaction.emoji.name === '✅';
+        const reactionFilter = (reaction, user) => reaction.emoji.name === "✅";
 
-        const embed = new Discord.RichEmbed({
-            title: 'Suggestion by someone',
-            description: 'This is a test suggestion. Can you please like it or dislike it :)',
-            fields: [
-                { name: 'Like:', value: '<3' }
-            ]
-        });
+        const pollEmbed = new Discord.RichEmbed()
+            .setColor("#EB671D")
+            .setTitle("Roast-Bot Help:")
+            .addBlankField()
+            .addField("Option 1: Yes")
+            .addField("Option 2: No")
+            .addField("Option 3: Maybe")
+            .setFooter("Vote by clicking on a reaction.")
 
-        // add reaction emoji to message
-        message.channel.send(embed)
-            .then(msg => msg.react('✅'))
-            .then(mReaction => mReaction.message.react('❎'))
+        message.channel.send(pollEmbed)
+            .then((msg) => msg.react("✅"))
+            .then((mReaction) => mReaction.message.react("❎"))
             .then(mReaction => {
-                // createReactionCollector - responds on each react, AND again at the end.
                 const collector = mReaction.message
                     .createReactionCollector(reactionFilter, { time: 15000 });
 
-                // set collector events
-                collector.on('collect', r => {
-                    // immutably copy embed's Like field to new obj
+                collector.on("collect", r => {
                     let embedLikeField = Object.assign({}, embed.fields[0]);
 
-                    // update 'field' with new value
-                    embedLikeField.value = '<3 <3 <3';
+                    embedLikeField.value = "<3 <3 <3";
 
-                    // create new embed with old title & description, new field
-                    const newEmbed = new Discord.RichEmbed({
-                        title: embed.title,
-                        description: embed.description,
-                        fields: [embedLikeField]
-                    });
+                    const newEmbed = new Discord.RichEmbed()
+                        .setColor("#EB671D")
+                        .setTitle("Roast-Bot Help:")
+                        .addBlankField()
+                        .addField("YESSS");
 
-                    // edit message with new embed
-                    // NOTE: can only edit messages you author
                     r.message.edit(newEmbed);
                     //await reactions.react("◀");
                     //await reactions.react("▶");
