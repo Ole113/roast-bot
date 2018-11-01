eb2878e328c22b6fea3ae0878f3e3409bf1e7a23
Ole113 <alex_elbel@icloud.com>
2018-11-01 08:47:06 -0600

    Update poll.js

--- Commands/poll.js
+++ Commands/poll.js
@@ -12,20 +12,6 @@ const prefixFile = require("../Database/prefix.json");
 exports.run = async (message) => {
     if (message.author.bot) { return; }
     if (message.content.toLowerCase().startsWith(prefixFile.prefix + "poll")) {
-
-        const pageOneEmbed = new Discord.RichEmbed({
-            title: "Roast-Bot Help:",
-            color: 15427357,
-            fields: [
-                { name: "\n\n*Commands:*\n\nr!help", value: "List of Roast-Bot Commands." },
-                { name: "r!roast *@user*, r!roast, or r!roast *roastNumber*", value: "Generate a random roast with the number of roast it was." },
-                { name: "r!meme, or r!meme *memeNumber*", value: "Sends a meme to the current channel." },
-                { name: "r!urban *whatToSearch*", value: "Search up anything on the Urban Dictionary!" },
-                { name: "r!invite", value: "Link to invite Roast-Bot to a server." },
-            ]
-        });
-        message.channel.send(pageOneEmbed)
-            .then(async mReaction => {
-            });
+        return message.channel.send("coming soon");
     }
 };
