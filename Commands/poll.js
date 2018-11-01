bac320e74baec571110852760ad84ce16e089f31
Ole113 <alex_elbel@icloud.com>
2018-11-01 08:36:00 -0600

    Update poll.js

--- Commands/poll.js
+++ Commands/poll.js
@@ -9,7 +9,7 @@ const Discord = require("discord.js");
 
 const prefixFile = require("../Database/prefix.json");
 
-exports.run = async (client, message) => {
+exports.run = async (message) => {
     if (message.author.bot) { return; }
     if (message.content.toLowerCase().startsWith(prefixFile.prefix + "poll")) {
 
