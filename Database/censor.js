928b7f8e6c9f05e1c2ea13e91aceb710e2ab4134
Ole113 <alex_elbel@icloud.com>
2018-11-09 09:02:09 -0700

    Update censor.js

--- Database/censor.js
+++ Database/censor.js
@@ -5,3 +5,16 @@
 *
 *
 */
+const Discord = require("discord.js");
+
+const prefixFile = require("../Database/prefix.json");
+const onOffFile = require("../Database/on-off.json");
+
+exports.run = async (message) => {
+  		if (message.author.bot) { return; }
+      if (message.content.toLowerCase().startsWith(prefixFile.prefix + "censor help") && onOffFile.censor == "on") {
+          return message.channel.send("On."); 
+      } else if (message.content.toLowerCase().startsWith(prefixFile.prefix + "censor help") && onOffFile.censor == "off") {
+          return message.channel.send("This command has been turned off by an administrator."); 
+      }
+}
