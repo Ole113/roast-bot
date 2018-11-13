2cb53940dda53fbf90ae3ab68682b1b558edb4fd
Ole113 <alex_elbel@icloud.com>
2018-11-13 08:25:05 -0700

    added String() to prefix

--- Database/on-off.js
+++ Database/on-off.js
@@ -2,14 +2,14 @@ const Enmap = require("enmap");
 const onOff = new Enmap({ name: "on-off" });
 const bsp = require("better-sqlite-pool");
  
-const prefixFile = require("./prefix.json");
- 
+const { prefixFile } = require("./prefix.json");
+
 exports.run = async (message) => {
 	onOff.defer.then(() => {
 		if (message.author.bot) { return; }
 
 		const key = message.guild.id;
-		const prefix = prefixFile.get(key, "prefix");
+		const prefix = String(prefixFile.get(key, "prefix"));
 
 		if (message.content.toLowerCase() === prefix + "off help" || message.content.toLowerCase() === prefix + "on help") {
 			return message.channel.send("**On-Off help:**\n\nTo use On-Off you first need to have admin permissions. Next you need to know which command to turn off, for this example we'll use roasts. Before the example of turning commands off remember that not all commamds can be turned off. The list of commands that cannot be turned off are: `r!invite`, `r!help`, custom prefixes, XP/Level, and the ability to turn commands on/off. Now for the example of turning roasts off:\n\nUSER: r!off roast\nRoast-Bot: Roast command has been turn off. User `r!on roast` to turn it back on.\n\nTurning a command back on after it having been turned off:\n\nUSER: r!on roast\nRoast-Bot: Roast command has been turn on. User `r!on roast` to turn it back off.\n\nAs long as you have \"roast\" the command will work, for example you could do `r!off roastsss` and the command would still work as long as you have roast in there.\n\n\nStill having trouble with on/off or have a suggestion? Join the support server: https://discordapp.com/invite/9y8yV42");
