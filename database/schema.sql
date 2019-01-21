9561849f579885a1ce943182d2b23e689458ddd0
Ole113 <alex_elbel@icloud.com>
2019-01-20 22:34:06 -0700

    aws rds connects now

---
+++ database/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE roast_bot_table (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username  TEXT         NOT NULL,
+    userID    TEXT         NOT NULL,
+    userXP    VARCHAR(100) NOT NULL,
+    userLevel VARCHAR(100) NOT NULL
+);
--- database/sqlXPTest.js
+++ database/sqlXPTest.js
@@ -1,32 +1,39 @@
-const Discord = require("discord.js");
-
 const mysql = require("mysql");
+const dbConfigFile = require("../dbConfig.json");
 
 let connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "roast_bot"
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT
 });
 
 connection.connect();
 
 exports.run = async (message) => {
-    if (message.author.bot) { return; }
 
     let user = {
-        username: message.author,
+        username: message.author.username,
         userID: message.author.id,
-        xp: 0,
-        level: 0
+        userXP: 0,
+        userLevel: 0
     }
-
-    let query = connection.query("insert into roast_bot set ?", user, function(err, result) {
-        if(err) {
+    
+    let query = connection.query("insert into roast_bot_xp set ?", user, function (err, result) {
+        if (err) {
             console.error(err);
             return;
         }
-        console.error(result);
+        user.userID++;
+        console.log(user.userXP);
+        //console.error(result);
     });
+
+    if(user.userXP >= 10) {
+        user.userLevel = 1;
+        console.log("LEVEL 1");
+    } else {
+        console.log(`LEVEL: ${user.userLevel}, XP: ${user.userXP}`);
+    }
 }
-exports.user = user;
