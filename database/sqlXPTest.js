a0497812ca8f9c5e84b9629895619e24c5e7890d
Ole113 <alex_elbel@icloud.com>
2019-01-21 21:09:44 -0700

    procfile removed

--- database/sqlXPTest.js
+++ database/sqlXPTest.js
@@ -15,26 +15,30 @@ exports.run = async (message) => {
     //INSERT INTO `t_users` VALUES ('1', 'Bonjour', 'mondeEEEEE');
 
     let user = {
-        username: message.author.username,
-        userID: message.author.id,
+        username: message.member.user.tag,
+        userID: message.member.id,
         userXP: 0,
         userLevel: 0
     }
-    
-    let query = connection.query("insert into roast_bot_xp set ?", user, function (err, result) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        user.userID++;
-        console.log(user.userXP);
-        //console.error(result);
-    });
 
-    if(user.userXP >= 10) {
-        user.userLevel = 1;
-        console.log("LEVEL 1");
-    } else {
-        console.log(`LEVEL: ${user.userLevel}, XP: ${user.userXP}`);
-    }
+    //SELECT * FROM roast_bot_xp WHERE userID = "390315922369282049";
+    let testQuery = connection.query(`SELECT * FROM roast_bot_xp WHERE userID = "${message.memeber.id}"`, function (err, result) {
+        if (result) {
+            let XP_Update_Query = connection.query(`UPDATE roast_bot_xp SET userXP = userXP + 1 WHERE userID = "${message.member.id}"`, function (err, result) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(result);
+            })
+        } else if (!result.length) {
+            let query = connection.query("INSERT INTO roast_bot_xp SET ?", user, function (err, result) {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.error(result);
+            });
+        }
+    });
 }
