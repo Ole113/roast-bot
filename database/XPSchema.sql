6c914abfcc552ee9172d81ee761c2a0f99cf5314
Ole113 <alex_elbel@icloud.com>
2019-01-24 14:41:55 -0700

    changes

--- database/XPSchema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE roast_bot_xp (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username  TEXT         NOT NULL,
-    userID    TEXT         NOT NULL,
-    userXP    VARCHAR(100) NOT NULL,
-    userLevel VARCHAR(100) NOT NULL
-);
