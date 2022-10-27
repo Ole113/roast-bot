838730ba1187f62a5b897feacf3f1a7f891f7cf6
Elgoat <Rune2576@gmail.com>
2022-10-27 08:25:32 +0200

    beautification of say.js (missed a new line)

--- commands/say.js
+++ commands/say.js
@@ -15,12 +15,13 @@ module.exports = {
                 // Shady work-around to avoid having to send a reply but also avoid the "interaction didn't respond" message.
                 await interaction.reply("if you can read this you're extremely fast and i'm fascinated by your fast reading skills!");
                 await interaction.deleteReply();
+                
                 message = interaction.options.getString('message')
                 try {
                     return await interaction.channel.send(message)
                 } catch (err) {
                     console.error(err)
-                    
+
                     // Error code 50013 is Missing permissions to channel
                     if (err.code == 50013) {
                         return await interaction.user.send(`Whoops. it looks like i'm unable to send messages in: ${interaction.guild.name} > #${interaction.channel.name}`)
